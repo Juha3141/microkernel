3b199a500e9c977f3c0482748bff081c0b0c9e1d
Ian Juha Cho <ianisnumber2027@gmail.com>
2023-11-12 18:55:52 +0900

    Update main.cpp

--- kernel/Sources/main.cpp
+++ kernel/Sources/main.cpp
@@ -1,13 +1,10 @@
-#include <memory_management.hpp>
+#include <kmem_manager.hpp>
 #include <debug.hpp>
 
 extern "C" void kernel_main(void) {
     Debug::Initialize();
     Debug::SetPosition(0 , 0);
     Debug::printf("Hello world!\n");
-    
-    memory::init();
-    // TaskManagement::Initialize();
     
     while(1) {
         ;

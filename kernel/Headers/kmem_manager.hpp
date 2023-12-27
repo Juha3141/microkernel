c8e18aa4827cab10cf69581ce504d2b423b4e6e7
Ian Juha Cho <ianisnumber2027@gmail.com>
2023-12-27 09:35:56 +0900

    Added full working physical memory allocator
    The "SegmentsManager" searches available NodesManager and allocates/releases physical memory (details in kmem_manager.cpp)

--- kernel/Headers/kmem_manager.hpp
+++ kernel/Headers/kmem_manager.hpp
@@ -17,13 +17,6 @@
 #define KERNEL_MEMORY_SEGMENT_THRESHOLD 512*1024  // 512KB
 
 namespace memory {
-     // SegmentsManager : Manager of segments, decide what segments to be used next
-    class SegmentsManager {
-        public:
-            
-        private:
-
-    };
     /*
     class MemoryMapManager {
         public:
@@ -39,15 +32,37 @@ namespace memory {
     void get_kstruct_boundary(struct Boundary &boundary);
 
     void determine_kstruct_boundary(struct Boundary &new_mboundary , struct KernelInfoStructure *kinfostruct);
-    int determine_pmem_boundary(struct Boundary protect , struct MemoryMap *new_mboundary , struct KernelInfoStructure *kinfostruct);
+    int determine_pmem_boundary(struct Boundary protect , struct Boundary *new_mboundary , struct KernelInfoStructure *kinfostruct);
 
     void kstruct_init(struct Boundary boundary);
     void *kstruct_alloc(max_t size , max_t alignment=0);
     
+    // SegmentsManager : Manager of segments, decide what segments to be used next
+    // Global class, use singleton pattern
+    struct SegmentsManager {
+        void init(int segment_count , struct Boundary *usable_segments);
+        static SegmentsManager *get_self(void) {
+            static SegmentsManager *ptr = 0x00;
+            if(ptr == 0x00) {
+                ptr = (SegmentsManager *)kstruct_alloc(sizeof(SegmentsManager));
+            }
+            return ptr;
+        }
+        int get_segment_index(max_t address);
+        
+        max_t get_currently_using_mem(void);
+        max_t total_memory;
+        int managers_count;
+        NodesManager *node_managers;
+    };
+    
     // pmem (physical memory) allocation
     void pmem_init(max_t memmap_count , struct MemoryMap *memmap , struct KernelInfoStructure *kinfostruct);
-    void *pmem_alloc(max_t size , max_t alignment=0);
+    ptr_t *pmem_alloc(max_t size , max_t alignment=0);
     void pmem_free(ptr_t *ptr);
+
+    max_t pmem_total_size(void);
+    max_t pmem_usage(void);
 }
 
 #endif
--- kernel/Sources/kmem_manager.cpp
+++ kernel/Sources/kmem_manager.cpp
@@ -24,18 +24,22 @@ void memory::get_kstruct_boundary(struct Boundary &boundary) {
 void memory::determine_kstruct_boundary(struct memory::Boundary &new_mboundary , struct KernelInfoStructure *kinfostruct) {
 	debug::push_function("d_kstruct_b");
 	struct MemoryMap *memmap = (struct MemoryMap *)kinfostruct->memmap_ptr;
-	max_t kernel_end_address = kinfostruct->kernel_address+kinfostruct->kernel_size;
+	max_t kernel_end_address = kinfostruct->total_kernel_area_end;
 	debug::out::printf("kernel_end_address : 0x%X\n" , kernel_end_address);
 	new_mboundary.start_address = align_address(kernel_end_address , 4096); // padding
-
-	// unnecessary but necessary I guess
+	/* The "kstruct" is location of all sorts of kernel structures that requires static memory location.
+	 * The kernel makes a space about 1MB large at the rear of kernel image for static structures.
+	 * This code (haha) basically searches what "memmap[i]" has the kernel image and calculates whether
+	 * the kstruct area is available to be created.
+	 */
 	for(int i = 0; i < kinfostruct->memmap_count; i++) {
 		max_t address = ((max_t)memmap[i].addr_high << 32)|memmap[i].addr_low;
 		max_t length = ((max_t)memmap[i].length_high << 32)|memmap[i].length_low;
-		debug::out::printf("seg%d : 0x%X~0x%X\n" , i , address , address+length);
+		// debug::out::printf("seg%d : 0x%X~0x%X\n" , i , address , address+length);
 		if(kernel_end_address >= address && kernel_end_address <= address+length
-		&& kinfostruct->kernel_address >= address && kinfostruct->kernel_address <= address+length) { // is kernel inside?
+		&& kinfostruct->total_kernel_area_start >= address && kinfostruct->total_kernel_area_start <= address+length) { // is kernel inside?
 			// determine boundary
+			// If segment cannot hold more than designated size, just use maximum size for the segment
 			new_mboundary.end_address = MIN(address+length , new_mboundary.start_address+KERNELSTRUCTURE_LENGTH);
 			debug::out::printf(DEBUG_INFO , "kstruct_boundary : 0x%X~0x%X\n" , new_mboundary.start_address , new_mboundary.end_address);
 			debug::pop_function();
@@ -48,7 +52,7 @@ void memory::determine_kstruct_boundary(struct memory::Boundary &new_mboundary ,
 	return;
 }
 
-int memory::determine_pmem_boundary(struct Boundary protect , struct MemoryMap *new_msegment_list , struct KernelInfoStructure *kinfostruct) {
+int memory::determine_pmem_boundary(struct Boundary protect , struct Boundary *new_msegment_list , struct KernelInfoStructure *kinfostruct) {
 	debug::push_function("d_pmem_b");
 
 	int seg_index = 0;
@@ -56,34 +60,42 @@ int memory::determine_pmem_boundary(struct Boundary protect , struct MemoryMap *
 	struct Boundary kstruct_boundary;
 	get_kstruct_boundary(kstruct_boundary);
 	// unnecessary but necessary I guess
+	/*
+	 * This is just parsing the protected area from the segment
+	 * Basically, this code excludes "protect" boundary from segment that encompasses that boundary
+	 * The segment splices the designated the segment(memmap[i]) into two parts which is usable(and not protected)
+	 * ..and saves the address of the segments to new_msegment_list
+	 * ..effectively excluding kernel boundary from physical memory pool
+	*/
 	for(int i = 0; i < kinfostruct->memmap_count; i++) {
 		max_t address = ((max_t)memmap[i].addr_high << 32)|memmap[i].addr_low;
 		max_t length = ((max_t)memmap[i].length_high << 32)|memmap[i].length_low;
-		// debug::out::printf("(d_pmem_b) seg%d : 0x%X~0x%X\n" , i , address , address+length);
+		// If the segment is larger than certain "threshold" -> save it 
 		if(protect.start_address >= address && protect.start_address <= address+length && protect.start_address-address >= KERNEL_MEMORY_SEGMENT_THRESHOLD) {
 			max_t new_addr = address;
 			max_t new_length = protect.start_address-address;
-			new_msegment_list[seg_index].addr_low = new_addr & 0xFFFFFFFF;
-			new_msegment_list[seg_index].addr_high = new_addr >> 32;
-			new_msegment_list[seg_index].length_low = new_length & 0xFFFFFFFF;
-			new_msegment_list[seg_index].length_high = new_length >> 32;
-			debug::out::printf(DEBUG_TEXT , "opt1 seg%d. 0x%X~0x%X\n" , seg_index , new_addr , new_addr+new_length);
+			
+			new_msegment_list[seg_index].start_address = new_addr;
+			new_msegment_list[seg_index].end_address = new_addr+new_length;
+			debug::out::printf("seg%d. 0x%X~0x%X\n" , seg_index , new_addr , new_addr+new_length);
 			seg_index++;
 
 		}
 		if(protect.end_address >= address && protect.end_address <= address+length) {
 			max_t new_addr = align_address(protect.end_address , 4096); // padding
 			max_t new_length = address+length-protect.end_address;
-			new_msegment_list[seg_index].addr_low = new_addr & 0xFFFFFFFF;
-			new_msegment_list[seg_index].addr_high = new_addr >> 32;
-			new_msegment_list[seg_index].length_low = new_length & 0xFFFFFFFF;
-			new_msegment_list[seg_index].length_high = new_length >> 32;
-			debug::out::printf(DEBUG_TEXT , "opt2 seg%d. 0x%X~0x%X\n" , seg_index , new_addr , new_addr+new_length);
+
+			new_msegment_list[seg_index].start_address = new_addr;
+			new_msegment_list[seg_index].end_address = new_addr+new_length;
+			debug::out::printf("seg%d. 0x%X~0x%X\n" , seg_index , new_addr , new_addr+new_length);
 			seg_index++;
 		}
 		else if(protect.start_address <= address+length) {
-			debug::out::printf("opt3 seg%d. 0x%X~0x%X\n" , seg_index , ((max_t)memmap[i].addr_high << 32)|(memmap[i].addr_low) , (((max_t)memmap[i].addr_high << 32)|(memmap[i].addr_low))+(((max_t)memmap[i].length_high << 32)|(memmap[i].length_low)));
-			memcpy(&new_msegment_list[seg_index++] , &memmap[i] , sizeof(struct MemoryMap));
+			debug::out::printf("seg%d. 0x%X~0x%X\n" , seg_index , address , address+length);
+			
+			new_msegment_list[seg_index].start_address = address;
+			new_msegment_list[seg_index].end_address = address+length;
+			seg_index++;
 		}
 	}
 	debug::pop_function();
@@ -104,34 +116,98 @@ void *memory::kstruct_alloc(max_t size , max_t alignment) {
 	return (void *)addr;
 }
 
+void memory::SegmentsManager::init(int segment_count , struct Boundary *usable_segments) {
+	debug::push_function("SgMgr::init");
+	managers_count = segment_count;
+	total_memory = 0;
+	// allocate space for all managers
+	node_managers = (NodesManager *)kstruct_alloc(managers_count*sizeof(NodesManager));
+	for(int i = 0; i < managers_count; i++) {
+		node_managers[i].init(usable_segments[i].start_address , usable_segments[i].end_address-usable_segments[i].start_address);
+		total_memory += (usable_segments[i].end_address-usable_segments[i].start_address);
+		debug::out::printf(DEBUG_SPECIAL , "why? %d. 0x%X~0x%X (0x%X)\n" , i , usable_segments[i].start_address , usable_segments[i].end_address , (usable_segments[i].end_address-usable_segments[i].start_address));
+	}
+	debug::pop_function();
+}
+
+int memory::SegmentsManager::get_segment_index(max_t address) {
+	for(int i = 0; i < managers_count; i++) {
+		if(address >= node_managers[i].mem_start_address && address <= node_managers[i].mem_end_address) {
+			return i;
+		}
+	}
+	return -1;
+}
+
 void memory::pmem_init(max_t memmap_count , struct MemoryMap *memmap , struct KernelInfoStructure *kinfostruct) {
 	debug::push_function("pmem_init");
 	
-	max_t total_memory = 0;
 	int usable_seg_count = 0;
 
 	struct Boundary kstruct_boundary;
-	struct MemoryMap pmem_boundary[memmap_count];
+	struct Boundary pmem_boundary[memmap_count];
 	int pmem_entry_count = 0;
 
 	// determine kernel struct address
 	determine_kstruct_boundary(kstruct_boundary , kinfostruct);
 	// determine physical memory address
-	if((pmem_entry_count = determine_pmem_boundary({kinfostruct->kernel_address , kstruct_boundary.end_address} , pmem_boundary , kinfostruct)) == 0) {
+	if((pmem_entry_count = determine_pmem_boundary({kinfostruct->total_kernel_area_start , kstruct_boundary.end_address} , pmem_boundary , kinfostruct)) == 0) {
 		debug::panic("kmem_manager.cpp" , 55 , "pmem_init() : zero detected usable memory\n");
-	}    
-	
-	kstruct_init(kstruct_boundary); // initialize kernel structure area
-	
-	struct NodesManager *node_mgr = (struct NodesManager *)kstruct_alloc(memmap_count*sizeof(struct NodesManager));
-	debug::out::printf(DEBUG_SPECIAL , "node_mgr : 0x%X\n" , (unsigned long)node_mgr);
-	for(int i = 0; i < pmem_entry_count; i++) {
-		max_t address = (max_t)pmem_boundary[i].addr_low|((max_t)pmem_boundary[i].addr_high << 32);
-		max_t length = (max_t)pmem_boundary[i].length_low|((max_t)pmem_boundary[i].length_high << 32);
-		total_memory += length;
-		debug::out::printf("0x%X ~ 0x%X\n" , address , address+length);
-
 	}
-	debug::out::printf("Total memory : %dMB\n" , total_memory/1024/1024);
+	kstruct_init(kstruct_boundary); // initialize kernel structure area
+	// allocate node manager for each segment
+	SegmentsManager *segments_mgr = SegmentsManager::get_self();
+	segments_mgr->init(pmem_entry_count , pmem_boundary);
+	debug::out::printf("Total memory : %dMB\n" , segments_mgr->total_memory/1024/1024);
 	debug::pop_function();
 }
+
+max_t memory::SegmentsManager::get_currently_using_mem(void) {
+	max_t currently_using_mem = 0;
+	for(int i = 0; i < managers_count; i++) {
+		currently_using_mem += node_managers[i].currently_using_mem;
+	}
+	return currently_using_mem;
+}
+
+ptr_t *memory::pmem_alloc(max_t size , max_t alignment) {
+	ptr_t *ptr = 0x00;
+	debug::push_function("pmem_alloc");
+	SegmentsManager *segments_mgr = SegmentsManager::get_self();
+	for(int i = 0; i < segments_mgr->managers_count; i++) {
+		if(!segments_mgr->node_managers[i].available()) continue;
+		if((ptr = (ptr_t *)segments_mgr->node_managers[i].allocate(size , alignment)) != 0x00) {
+			break;
+		}
+	}
+	if(ptr == 0x00) {
+		debug::panic("no available physical memory\n");
+	}
+	debug::pop_function();
+	return ptr;
+}
+
+void memory::pmem_free(ptr_t *ptr) {
+	debug::push_function("pmem_free");
+	SegmentsManager *segments_mgr = SegmentsManager::get_self();
+	int index;
+	if((index = segments_mgr->get_segment_index((max_t)ptr)) == -1) {
+		debug::out::printf("Warning : Memory release request out of range(0x%X)\n" , (max_t)ptr);
+		debug::pop_function();
+		return;
+	}
+	if(segments_mgr->node_managers[index].free((max_t)ptr) == false) {
+		debug::out::printf("Warning : Memory release request not allocated(0x%X)\n" , (max_t)ptr);
+	}
+	debug::pop_function();
+}
+
+max_t memory::pmem_usage(void) {
+	SegmentsManager *segments_mgr = SegmentsManager::get_self();
+	return segments_mgr->get_currently_using_mem();
+}
+
+/* To-do (2023.12.27) : 
+ * 2. What about higher-half kernel???
+ * 3. turn on fPIC (for only debugging : use fixed address)  
+*/

a9552743718b21d7b0b949f4d3b96fb236025195
Ian Juha Cho <ianisnumber2027@gmail.com>
2023-12-28 10:34:08 +0900

    Update common.mk
    Changed compile option for higher-half kernel
    + Removed unnecessary options

--- kernel/common.mk
+++ kernel/common.mk
@@ -6,5 +6,10 @@ ASM = nasm
 OBJCOPY = x86_64-elf-objcopy
 
 # -fPIC : Position Independent Code
-CCOPTIONS = -g -ffreestanding -fpack-struct=1  -masm=intel -nostdlib -Werror=return-type -fno-stack-protector -fno-use-cxa-atexit -fno-threadsafe-statics -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings
+CCOPTIONS = -g -ffreestanding \
+-mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 \
+-fpack-struct=1  -masm=intel -nostdlib \
+-Werror=return-type -fno-stack-protector \
+-fno-use-cxa-atexit -fno-threadsafe-statics \
+-fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings
 LDOPTIONS = -m elf_x86_64

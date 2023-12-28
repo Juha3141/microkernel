d23541c4b0d2b255133b2e1d48135c6ecbb9f3c9
Ian Juha Cho <ianisnumber2027@gmail.com>
2023-12-28 10:42:41 +0900

    Added function for page relocation
    Added function for page relocation for higher-half kernel relocation
    + Fixed bugs

--- loader/x32/Headers/intel_paging.h
+++ loader/x32/Headers/intel_paging.h
@@ -3,6 +3,8 @@
 
 #include <../../../kernel/Headers/kernel_argument.hpp>
 
+#define PAGE_SIZE                0x200000
+
 #define PAGE_MAX_ENTRY_COUNT     512
 #define PAGE_PML4ENTRY_FLAGS_P   0b000000000001
 #define PAGE_PML4ENTRY_FLAGS_RW  0b000000000010
@@ -35,17 +37,22 @@
 #define PAGE_PDENTRY_FLAGS_G     0b00000100000000
 #define PAGE_PDENTRY_FLAGS_EXB   0b10000000000000
 
-typedef struct PageEntry {
-    unsigned short Flags:12;
-    unsigned int BaseAddressLow:32;  // Total 52bit, Note that base address should be 
-    unsigned int BaseAddressHigh:20; // aligned to 4KB
-}
-PageEntry , 
-PageDirectoryPointerEntry , 
-PageDirectoryEntry , 
-PageTableEntry;
+#define PAGE_GET_PML4ENTRY_NUM(addr)  (((addr) >> 39) & 0x1FF)
+#define PAGE_GET_PDPTENTRY_NUM(addr)  (((addr) >> 30) & 0x1FF)
+#define PAGE_GET_PDENTRY_NUM(addr)    (((addr) >> 21) & 0x1FF)
+#define PAGE_GET_OFFSET(addr)         ((addr) & 0x1FFFFF) 
+
+struct PageEntry {
+    unsigned int BaseAddressLow_Flags;  // Total 52bit, Note that base address should be aligned to 4KB
+    unsigned int BaseAddressHigh;
+};
 
 void SetPageEntry(struct PageEntry *PageEntry , unsigned int BaseAddressLow , unsigned int BaseAddressHigh , unsigned short Flags);
 unsigned int SetupPML4_custom(unsigned int start_address , struct MemoryMap *memmap);
+void ModifyPML4Entry(unsigned int entry_addr , unsigned int pml4_entry , unsigned int base_low , unsigned int base_high , unsigned short flags);
+void ModifyPDPTEntry(unsigned int entry_addr , unsigned int pml4_entry , unsigned int pdpt_entry , unsigned int base_low , unsigned int base_high , unsigned short flags);
+void ModifyPDEntry(unsigned int entry_addr , unsigned int pml4_entry , unsigned int pdpt_entry , unsigned int pd_entry , unsigned int base_low , unsigned int base_high , unsigned short flags);
+
+void RelocatePage(unsigned int kernel_address , unsigned int kernel_page_size , unsigned int new_address , unsigned int pml4_entry_address , unsigned short new_flags);
 
 #endif
--- loader/x32/Sources/intel_paging.c
+++ loader/x32/Sources/intel_paging.c
@@ -1,36 +1,81 @@
-#include <intel_paging.h>
+ #include <intel_paging.h>
 
 void SetPageEntry(struct PageEntry *PageEntry , unsigned int BaseAddressLow , unsigned int BaseAddressHigh , unsigned short Flags) {
-    PageEntry->BaseAddressLow = (BaseAddressLow >> 12)|(BaseAddressHigh<<(32-12));
-    PageEntry->BaseAddressHigh = BaseAddressHigh >> 12;
-    if((Flags & (1 << 13)) == (1 << 13)) {  // Set PAT Flag
-        PageEntry->BaseAddressLow |= 0x01;
-    }
-    if((Flags & (1 << 14)) == (1 << 14)) {  // Set EXB Flag
-        PageEntry->BaseAddressHigh |= (1 << 20);
-    }
-    PageEntry->Flags = Flags & 0xFFF;
+    PageEntry->BaseAddressLow_Flags = (BaseAddressLow & 0xFFFFF000)|(Flags & 0xFFF);
+    PageEntry->BaseAddressHigh = BaseAddressHigh;
 }
 
 unsigned int SetupPML4_custom(unsigned int start_address , struct MemoryMap *memmap) {
-    unsigned int pml4_entry_address = start_address; // total 512 entries
-    unsigned int pdpt_entry_address = pml4_entry_address+(PAGE_MAX_ENTRY_COUNT*sizeof(struct PageEntry)); // total 512 entries
-    unsigned int pde_entry_address = pdpt_entry_address+(PAGE_MAX_ENTRY_COUNT*sizeof(struct PageEntry));
+    // The entries exist linearly (adjacent to each other)
+    // PML4 -> PDPT -> PD, 512 entries each
+    // one PML4 consists 512 PDPT entry, one PDPT entry consists 512 PD entry
+    
+    // Total size : PML4(512*8) + PDPT(512*8*1) + PD(512*512*8*1) = 0x202000 (approx. 2MB)
+    
+    struct PageEntry *pml4_entry = (struct PageEntry *)start_address; // total 512 entries
+    struct PageEntry *pdpt_entry = pml4_entry+(PAGE_MAX_ENTRY_COUNT*sizeof(struct PageEntry)); // total 512 entries
+    struct PageEntry *pde_entry = pdpt_entry+(PAGE_MAX_ENTRY_COUNT*sizeof(struct PageEntry));
 
     unsigned int physicaladdr_low = 0;
     unsigned int physicaladdr_high = 0;
     for(int i = 0; i < PAGE_MAX_ENTRY_COUNT; i++) {
-        SetPageEntry(pml4_entry_address+(i*sizeof(struct PageEntry)) , 0 , 0 , 0);
+        SetPageEntry(&(pml4_entry[i]) , 0 , 0 , 0);
     }
-    SetPageEntry(pml4_entry_address , pdpt_entry_address , 0x00 , PAGE_PML4ENTRY_FLAGS_P|PAGE_PML4ENTRY_FLAGS_RW);
-    for(int i = 0 , j = 0; i < PAGE_MAX_ENTRY_COUNT; i++) {
-        SetPageEntry(pdpt_entry_address+(i*sizeof(struct PageEntry)) , pde_entry_address , 0x00 , PAGE_PDPTENTRY_FLAGS_P|PAGE_PDPTENTRY_FLAGS_RW);
-        for(int j = 0; j < PAGE_MAX_ENTRY_COUNT; j++) {
-            SetPageEntry(pde_entry_address+(j*sizeof(struct PageEntry)) , physicaladdr_low , physicaladdr_high , PAGE_PDENTRY_FLAGS_P|PAGE_PDENTRY_FLAGS_RW|PAGE_PDENTRY_FLAGS_PS);
-            physicaladdr_low += 0x200000;
-            physicaladdr_high += (physicaladdr_low == 0);
+    unsigned int k = 1;
+    SetPageEntry(&(pml4_entry[0]) , pdpt_entry , 0x00 , PAGE_PML4ENTRY_FLAGS_P|PAGE_PML4ENTRY_FLAGS_RW);
+    for(unsigned int i = 0 , j = 0; i < 32; i++) {
+        SetPageEntry(&(pdpt_entry[i]) , pde_entry , 0x00 , PAGE_PDPTENTRY_FLAGS_P|PAGE_PDPTENTRY_FLAGS_RW);
+        
+        for(unsigned int j = 0; j < PAGE_MAX_ENTRY_COUNT; j++) {
+            SetPageEntry(&(pde_entry[j]) , ((unsigned int)j << 21)|(((unsigned int)i & 0b11) << 30) , ((unsigned int)i >> 2) , PAGE_PDENTRY_FLAGS_P|PAGE_PDENTRY_FLAGS_RW|PAGE_PDENTRY_FLAGS_PS);
+            /*if(((j << 21)|((i & 0b11) << 30)) == 0xC0000000) {
+                SetPageEntry(&(pde_entry[j]) , 0xC0000000 , k , PAGE_PDENTRY_FLAGS_P|PAGE_PDENTRY_FLAGS_RW|PAGE_PDENTRY_FLAGS_PS);
+                k += 1;
+            }*/
         }
-        pde_entry_address += PAGE_MAX_ENTRY_COUNT*sizeof(struct PageEntry);
+        pde_entry += PAGE_MAX_ENTRY_COUNT*sizeof(struct PageEntry);
     }
-    return pde_entry_address;
+    return (unsigned int)pde_entry;
+}
+
+void ModifyPML4Entry(unsigned int entry_addr , unsigned int pml4_entry , unsigned int base_low , unsigned int base_high , unsigned short flags) {
+    unsigned int pml4_entry_address = entry_addr; // total 512 entries
+    struct PageEntry *t_pml4_entry = (struct PageEntry *)pml4_entry_address;
+
+    SetPageEntry(&(t_pml4_entry[pml4_entry]) , base_low , base_high , flags);
+}
+
+void ModifyPDPTEntry(unsigned int entry_addr , unsigned int pml4_entry , unsigned int pdpt_entry , unsigned int base_low , unsigned int base_high , unsigned short flags) {
+    unsigned int pml4_entry_address = entry_addr; // total 512 entries
+    struct PageEntry *t_pml4_entry = (struct PageEntry *)pml4_entry_address;
+    struct PageEntry *t_pdpt_entry = (struct PageEntry *)(t_pml4_entry[pml4_entry].BaseAddressLow_Flags & 0xFFFFF000);
+    
+    SetPageEntry(&(t_pdpt_entry[pdpt_entry]) , base_low , base_high , flags);
+}
+
+void ModifyPDEntry(unsigned int entry_addr , unsigned int pml4_entry , unsigned int pdpt_entry , unsigned int pd_entry , unsigned int base_low , unsigned int base_high , unsigned short flags) {
+    unsigned int pml4_entry_address = entry_addr; // total 512 entries
+    struct PageEntry *t_pml4_entry = (struct PageEntry *)pml4_entry_address;
+    struct PageEntry *t_pdpt_entry = (struct PageEntry *)(t_pml4_entry[pml4_entry].BaseAddressLow_Flags & 0xFFFFF000);
+    struct PageEntry *t_pd_entry = (struct PageEntry *)(t_pdpt_entry[pdpt_entry].BaseAddressLow_Flags & 0xFFFFF000);
+
+    SetPageEntry(&(t_pd_entry[pd_entry]) , base_low , base_high , flags);
+}
+
+void RelocatePage(unsigned int kernel_address , unsigned int kernel_page_size , unsigned int new_address , unsigned int pml4_entry_address , unsigned short new_flags) {
+    unsigned int pml4e_index = PAGE_GET_PML4ENTRY_NUM(new_address);
+    unsigned int pdpte_index = PAGE_GET_PDPTENTRY_NUM(new_address);
+    unsigned int pde_index = PAGE_GET_PDENTRY_NUM(new_address);
+    unsigned int offset = PAGE_GET_OFFSET(new_address);
+    PrintString(0x07 , "Setting base address of page... ");
+    PrintString(0x07 , "pml4 index : %d | pdpt index : %d | pd_index : %d\n" , pml4e_index , pdpte_index , pde_index);
+    PrintString(0x07 , "new base address : 0x%X\n" , kernel_address);
+    for(unsigned int i = 0; i < kernel_page_size; i++) {
+        if(pde_index >= 511) {
+            pde_index = 0;
+            pdpte_index += 1;
+        } 
+        ModifyPDEntry(pml4_entry_address , pml4e_index , pdpte_index , pde_index , kernel_address+(PAGE_SIZE*i) , 0x00 , new_flags);
+        pde_index += 1;
+    }
 }

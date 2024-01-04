4483df6a456d32a6cbe38c5312a22561c42f7053
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-01-04 18:02:54 +0900

    Added abstract interrupt system

--- kernel/Headers/interrupt.hpp
+++ kernel/Headers/interrupt.hpp
@@ -1,6 +1,64 @@
+/**
+ * @file interrupt.hpp
+ * @author Ian Juha Cho(ianisnumber2027@gmail.com)
+ * @brief Generalized interrupt manager for global kernel
+ * @date 2023-12-30
+ * 
+ * @copyright Copyright (c) 2023 Ian Juha Cho
+ * 
+ */
+
 #ifndef _INTERRUPT_HPP_
 #define _INTERRUPT_HPP_
 
+#include <interface_type.hpp>
+#include <interrupt_hardware.hpp>
+#include <kmem_manager.hpp>
 
+#define INTERRUPT_HANDLER_EXCEPTION    0b00000001
+#define INTERRUPT_HANDLER_HARDWARE     0b00000010
+#define INTERRUPT_HANDLER_SOFTWARE     0b00000100
+#define INTERRUPT_HANDLER_LEVEL_KERNEL 0b00001000
+#define INTERRUPT_HANDLER_LEVEL_USER   0b00010000
+
+#define INTERRUPT_MAXCOUNT 255
+
+namespace interrupt {
+    struct Interrupt {
+        ptr_t handler;
+        word option;
+    };
+    // software interrupt manager
+    struct InterruptManager {
+        void init(void);
+        SINGLETON_PATTERN_KSTRUCT(struct InterruptManager);
+        
+        bool register_interrupt(int number , ptr_t handler , word interrupt_option);
+        ptr_t discard_interrupt(int number);
+        
+        void mask_interrupt(int number) { mask_flag[number] = true; }
+        void unmask_interrupt(int number) { mask_flag[number] = false; }
+        bool is_masked(int number) { return mask_flag[number]; }
+        
+        bool mask_flag[INTERRUPT_MAXCOUNT];
+        Interrupt interrupt_list[INTERRUPT_MAXCOUNT];
+    };
+    struct SpecialInterruptManager {
+        void init(void);
+        SINGLETON_PATTERN_KSTRUCT(struct SpecialInterruptManager);
+        
+        bool register_interrupt(char *int_name , ptr_t handler , word interrupt_option);
+        ptr_t discard_interrupt(char *int_name);
+    };
+    // interrupt_handler_common : 
+    void init(void);
+    
+    bool register_interrupt(int number , ptr_t handler_ptr , word interrupt_option); // Register interrupt handler
+    bool discard_interrupt(int number);                      // Discard interrupt handler
+    bool mask_interrupt(int number);
+    bool unmask_interrupt(int number);
+
+    extern "C" void interrupt_handler_common(max_t stack_address , struct SavedRegisters *saved_regs , int int_num);
+}
 
 #endif
---
+++ kernel/Headers/interrupt_hardware.hpp
@@ -0,0 +1,31 @@
+#ifndef _INTERRUPT_HARDWARE_HPP_
+#define _INTERRUPT_HARDWARE_HPP_
+
+#include <interface_type.hpp>
+#include <interrupt_specification.hpp>
+
+namespace interrupt {
+    namespace hardware {
+        void init(void);   // Initialize interrupt in hardware
+
+        void enable(void);
+        void disable(void);
+        
+        bool register_interrupt(int number , ptr_t handler_ptr , word interrupt_option); // Register interrupt handler
+        bool discard_interrupt(int number);                      // Discard interrupt handler
+
+        bool mask_interrupt(int number);
+        bool unmask_interrupt(int number);
+
+        void interrupt_received(void);        // EOI
+
+        extern "C" void save_registers(struct SavedRegister *regs);  // Save register routine
+        extern "C" void load_registers(struct SavedRegister *regs);  // Load register routine
+    };
+};
+
+namespace exception {
+    
+};
+
+#endif

b6f97365332696b805742476e19ce43288fd0fb8
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-01-04 17:57:29 +0900

    Update segmentation.cpp
    Added actual segmentations

---
+++ kernel/Sources/segmentation.cpp
@@ -0,0 +1,93 @@
+#include <segmentation.hpp>
+#include <segmentation_hardware.hpp>
+#include <kmem_manager.hpp>
+#include <kernel_info.hpp>
+
+#include <string.hpp>
+#include <debug.hpp>
+
+void segmentation::SegmentsManager::init(int entry_count) {
+    segments_data = (s_data_t *)memory::kstruct_alloc(entry_count*sizeof(s_data_t));
+    for(int i = 0; i < entry_count; i++) {
+        segments_data[i].occupied = false;
+    }
+    segments_count = 0;
+    segments_maxcount = entry_count;
+}
+
+void segmentation::SegmentsManager::register_segment(const char *segment_name , segment_t segment_value) {
+    debug::push_function("SegMgr::reg_seg");
+    int index = 0;
+    for(; index < segments_maxcount; index++) {
+        if(segments_data[index].occupied == false) break;
+    }
+    if(index >= segments_maxcount) {
+        // warning
+        debug::pop_function();
+        return;
+    }
+    debug::out::printf("registering segment \"%s\" to index %d -- " , segment_name , index);
+    segments_data[index].occupied = true;
+    strcpy(segments_data[index].name , segment_name);
+    debug::out::raw_printf("%s\n" , segments_data[index].name);
+    segments_data[index].value = segment_value;
+    segments_count++;
+    debug::pop_function();
+}
+
+segment_t segmentation::SegmentsManager::discard_segment(const char *segment_name) {
+    int index = 0;
+    for(; index < segments_maxcount; index++) {
+        if(strcmp(segments_data[index].name , segment_name) == 0) break;
+    }
+    if(index >= segments_maxcount) {
+        return SEGMENT_VALUE_INVALID;
+    }
+    segments_data[index].occupied = false;
+    memset(segments_data[index].name , 0 , sizeof(segments_data[index].name));
+    segments_count--;
+    return segments_data[index].value;
+}
+
+void segmentation::init(void) {
+    debug::push_function("seg::init");
+    struct kernel_segments_info kseginfo;
+    struct kernel_segments_value ksegvalue;
+    SegmentsManager *segment_mgr = SegmentsManager::get_self();
+    segment_mgr->init(SEGMENT_MAXCOUNT);
+    
+    segmentation::hardware::customize_segmentation_model(kseginfo);
+
+    // Initialize both hardware/software
+    segmentation::hardware::init(kseginfo , ksegvalue);
+    segment_mgr->register_segment(SEGMENT_NAME_CODE , ksegvalue.kernel_code);
+    segment_mgr->register_segment(SEGMENT_NAME_DATA , ksegvalue.kernel_data);
+    segment_mgr->register_segment(SEGMENT_NAME_USER_CODE , ksegvalue.user_code);
+    segment_mgr->register_segment(SEGMENT_NAME_USER_DATA , ksegvalue.user_data);
+    debug::out::printf("segment_mgr : 0x%lX\n" , (max_t)segment_mgr);
+
+    debug::out::printf(DEBUG_SPECIAL , "current segment count : %d\n" , segment_mgr->segments_count);
+    for(int i = 0; i < segment_mgr->segments_maxcount; i++) {
+        if(segment_mgr->segments_data[i].occupied == false) continue;
+        debug::out::printf(DEBUG_SPECIAL , "%d. \"%s\", value:0x%02x\n" , i , segment_mgr->segments_data[i].name , segment_mgr->segments_data[i].value);
+    }
+    debug::pop_function();
+}
+
+void segmentation::register_segment(const char *segment_name , max_t start_address , max_t length , word segment_type) {
+    segment_t segment = segmentation::hardware::register_segment(start_address , length , segment_type);
+    SegmentsManager::get_self()->register_segment(segment_name , segment);
+}
+
+void segmentation::discard_segment(const char *segment_name) {
+    segment_t segment = SegmentsManager::get_self()->discard_segment(segment_name);
+    segmentation::hardware::discard_segment(segment);
+}
+
+void segmentation::set_to_code_segment(const char *segment_name , ptr_t new_point) {
+    
+}
+
+void segmentation::set_to_data_segment(const char *segment_name) {
+
+}

526f0915688d479454425a6b589b4376d7774122
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-01-05 15:31:01 +0900

    Update interrupt.cpp
    Added temporary interrupt handler for testing

--- kernel/Sources/interrupt.cpp
+++ kernel/Sources/interrupt.cpp
@@ -1,5 +1,6 @@
 #include <interrupt.hpp>
 #include <string.hpp>
+#include <io_port.hpp>
 
 #include <debug.hpp>
 
@@ -9,9 +10,7 @@ void interrupt::InterruptManager::init(void) {
 }
 
 bool interrupt::InterruptManager::register_interrupt(int number , ptr_t handler , word interrupt_option) {
-    if(interrupt_list[number].handler != 0x00) {
-        return false;
-    }
+    // Allow interrupt overriding
     interrupt_list[number].handler = handler;
     interrupt_list[number].option = interrupt_option;
     return true;
@@ -24,9 +23,35 @@ ptr_t interrupt::InterruptManager::discard_interrupt(int number) {
     return handler;
 }
 
+/*
+
+__attribute__ ((naked)) void uninitialized_interrupt_handler_8(void) {
+    debug::push_function("fucktest");
+    debug::out::printf(DEBUG_ERROR , "Double Fault #8\n");
+    debug::pop_function();
+    
+    __asm__ ("iretq");
+}
+
+__attribute__ ((naked)) void uninitialized_interrupt_handler_13(void) {
+    debug::push_function("fucktest");
+    debug::panic("General Protection Fault #13\n");
+    while(1) {
+        ;
+    }
+}
+
+__attribute__ ((naked)) void uninitialized_interrupt_handler_32(void) {
+    io_write_byte(0x20 , 0x20);
+    __asm__ ("iretq");
+}
+
+*/
+
 void interrupt::init(void) {
     InterruptManager::get_self()->init();
-    hardware::init();
+    interrupt::hardware::init();
+    // To-do : Get some handlers
 }
 
 bool interrupt::register_interrupt(int number , ptr_t handler_ptr , word interrupt_option) {

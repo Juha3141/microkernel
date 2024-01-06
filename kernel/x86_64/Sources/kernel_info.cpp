141ed85ebcbe5a9e9e2b8a5dd0c8e11ca0547fc9
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-01-06 12:43:22 +0900

    Update kernel_info.cpp

--- kernel/x86_64/Sources/kernel_info.cpp
+++ kernel/x86_64/Sources/kernel_info.cpp
@@ -2,14 +2,15 @@
 #include <debug.hpp>
 
 void set_initial_kernel_info(void) {
-    KernelInfo *kinfo = KernelInfo::get_self();
+    KernelInfo *kinfo = GLOBAL_KERNELINFO;
     memset(kinfo , 0 , sizeof(KernelInfo));
     
     /* Segmentation related */
-    kinfo->predet.segmentation_mode = KINFO_SEGMENTATION_MODE_LINEAR_CORRESPONDENCE;
-    kinfo->predet.use_ist = true;
+    kinfo->conditions.task_segmentation_mode = KINFO_TASKSEG_LINEAR_CORRESPONDENCE;
+    kinfo->conditions.use_ist = true;
 
     /* Multicore system related */
-    kinfo->predet.multicore_usage = Using;
+    kinfo->conditions.multicore_usage = Using;
     
+    debug::out::printf("use_ist : %d\n" , kinfo->conditions.use_ist);
 }

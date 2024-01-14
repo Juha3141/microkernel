f5b202308a7b825106798b8f7f66aca670cb3296
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-01-14 23:36:08 +0900

    Update interrupt_hardware.hpp
    Removed unnecessary codes

--- kernel/Headers/interrupt_hardware.hpp
+++ kernel/Headers/interrupt_hardware.hpp
@@ -7,21 +7,14 @@
 namespace interrupt {
     namespace hardware {
         void init(void);   // Initialize interrupt in hardware
-
+        
         void enable(void);
         void disable(void);
         
         bool register_interrupt(int number , ptr_t handler_ptr , word interrupt_option); // Register interrupt handler
         bool discard_interrupt(int number);                      // Discard interrupt handler
-
-        void set_interrupt_mask(int number , bool masked);
-
-        void interrupt_received(void);        // EOI
-
-        extern "C" void save_registers(struct SavedRegister *regs);  // Save register routine
-        extern "C" void load_registers(struct SavedRegister *regs);  // Load register routine
-    };
-};
+    }
+}
 
 namespace exception {
     

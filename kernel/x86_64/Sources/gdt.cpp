f7e315a761f6f1d1b6f9a7e137c917b72d25de0d
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-01-14 23:32:11 +0900

    Update gdt.cpp
    Changed tss to ldt

--- kernel/x86_64/Sources/gdt.cpp
+++ kernel/x86_64/Sources/gdt.cpp
@@ -9,7 +9,7 @@ void x86_64::gdt::convert_type_flags(word segment_type , byte &type , byte &flag
         type |= GDT_TYPE_E; // executable
     }
     if((segment_type & SEGMENT_TYPE_TASK_SEGMENT) == SEGMENT_TYPE_TASK_SEGMENT) {
-        flags |= GDT_TYPE_LDT;
+        type |= GDT_TYPE_LDT;
     }
     if((segment_type & SEGMENT_TYPE_USER_PRIVILEGE) == SEGMENT_TYPE_USER_PRIVILEGE) {
         flags |= GDT_FLAGS_DPL3;
@@ -50,16 +50,16 @@ int x86_64::gdt::register_gdt(dword base_address , dword limit , byte type , byt
 int x86_64::gdt::register_ldt(qword base_address , dword limit , byte type , byte flags) {
     GDTContainer *gdt_container = GDTContainer::get_self();
     int index = gdt_container->current_index;
-    LDTEntry *tss_entry = (LDTEntry *)&(gdt_container->entries[index]);
-    tss_entry->base_low = base_address & 0xFFFFFFF;
-    tss_entry->base_high = base_address >> 24;
-    tss_entry->limit_low = limit & 0xFFFF;
-    tss_entry->limit_high = limit >> 16;
-    tss_entry->flags_low = flags & 0x0F;
-    tss_entry->flags_high = flags >> 4;
-    tss_entry->type = type & 0x0F;
+    LDTEntry *ldt_entry = (LDTEntry *)&(gdt_container->entries[index]);
+    ldt_entry->base_low = base_address & 0xFFFFFFF;
+    ldt_entry->base_high = base_address >> 24;
+    ldt_entry->limit_low = limit & 0xFFFF;
+    ldt_entry->limit_high = limit >> 16;
+    ldt_entry->flags_low = flags & 0x0F;
+    ldt_entry->flags_high = flags >> 4;
+    ldt_entry->type = type & 0x0F;
     
     // double the size
-    gdt_container->current_index += 4;
+    gdt_container->current_index += 3;
     return index;
 }

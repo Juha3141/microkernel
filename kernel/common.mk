92c0a80f60dd4dde2d3ac573132a82bfceb82dc0
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-02-04 20:34:45 +0900

    Update common.mk

--- kernel/common.mk
+++ kernel/common.mk
@@ -3,12 +3,12 @@ HEADERSFOLDER = Headers
 
 CC = x86_64-elf-g++
 ASM = nasm
+LD = x86_64-elf-ld
 OBJCOPY = x86_64-elf-objcopy
 
-# -fPIC : Position Independent Code
 CCOPTIONS = -g -ffreestanding \
 -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 \
--fpack-struct=1  -masm=intel -nostdlib \
+-fpack-struct=1 -masm=intel -nostdlib \
 -Werror=return-type -fno-stack-protector \
 -fno-use-cxa-atexit -fno-threadsafe-statics \
 -fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings

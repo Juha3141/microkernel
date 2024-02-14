a21bf384d7ffb6f91d2321168b085d1b929855cb
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-02-15 02:05:21 +0900

    Changed "storage device driver" to "block device driver"

--- kernel/Headers/drivers/block_device_driver.hpp
+++ kernel/Headers/drivers/block_device_driver.hpp
@@ -2,7 +2,26 @@
 #define _BLOCK_DEVICE_DRIVER_HPP_
 
 #include <interface_type.hpp>
-#include <drivers/storage_device_driver.hpp>
+#include <object_manager.hpp>
+#include <linked_list.hpp>
+#include <interrupt.hpp>
+
+typedef max_t resource_flag_t;
+typedef max_t etc_resource_t;
+
+struct device_resources {
+    int io_port_count;
+    io_port *io_ports;
+
+    int interrupt_count;
+    interrupt::interrupt_info_t *interrupts;
+    
+    int flags_count;
+    resource_flag_t *flags;
+
+    int etc_resources_count;
+    etc_resource_t *etc_resources;
+};
 
 namespace blockdev {
     // pre-define
@@ -15,17 +34,46 @@ namespace blockdev {
         max_t driver_id;
     };
 
+    typedef struct partition_info_s {
+        max_t physical_sector_start;
+        max_t physical_sector_end;
+
+        bool bootable;
+    }partition_info_t;
+
+    typedef enum storage_type_s {
+        physical=0 , logical=1
+    }storage_type_t;
+
+    struct storage_info_t {
+        max_t partition_driver_id;
+        storage_type_t storage_type;
+        
+        /* For logical storage */
+        max_t partition_id;
+        // If physical_sector_start == 0 --> Physical disk, "physical_sector_end" describes the total sector count
+        // If physical_sector_start != 0 --> Logical disk
+        partition_info_t partition_info; 
+
+        /* Common */
+        blockdev::BlockDeviceContainer *logical_block_devs;
+    };
     struct block_device {
         max_t id;
         block_device_driver *device_driver;
         device_resources resource;
+        
+        storage_info_t storage_info;
+
+        max_t mount_id;
+        // FileSystemDriver *fs_driver;
     };
     struct block_device_driver {
         virtual bool prepare(void) = 0;
         virtual max_t read(block_device *device , max_t block_address , max_t count , void *buffer) = 0;
         virtual max_t write(block_device *device , max_t block_address , max_t count , void *buffer) = 0;
-        virtual bool io_read(block_device *device , max_t command , ...) = 0;
-        virtual bool io_write(block_device *device , max_t command , ...) = 0;
+        virtual bool io_read(block_device *device , max_t command , max_t argument) = 0;
+        virtual bool io_write(block_device *device , max_t command , max_t argument) = 0;
 
         class BlockDeviceContainer *device_container;
         block_device_driver_info driver_info;
@@ -46,6 +94,25 @@ namespace blockdev {
             }
     };
     void init(void);
+
+    max_t register_driver(block_device_driver *driver , const char *driver_name);
+    block_device_driver *search_driver(const char *driver_name);
+    block_device_driver *search_driver(max_t driver_id);
+
+    max_t discard_driver(const char *driver_name);
+    max_t discard_driver(max_t driver_id);
+    
+    max_t register_device(block_device_driver *driver , block_device *device);
+    max_t register_device(const char *driver_name , block_device *device);
+    max_t register_device(max_t driver_id , block_device *device);
+    block_device *search_device(block_device_driver *driver , max_t device_id);
+    block_device *search_device(const char *driver_name , max_t device_id);
+    block_device *search_device(max_t driver_id , max_t device_id);
+
+    bool discard_device(block_device *device);
+
+    block_device *create_empty_device(block_device_driver *driver);
+    void designate_resources_count(block_device *device , int io_port_count , int interrupt_count , int flags_count , int etc_res_count);
 };
 
 #endif
--- kernel/Sources/drivers/block_device_driver.cpp
+++ kernel/Sources/drivers/block_device_driver.cpp
@@ -1,5 +1,94 @@
 #include <drivers/block_device_driver.hpp>
+#include <linked_list.hpp>
+
+#include <kmem_manager.hpp>
+
+#include <debug.hpp>
 
 void blockdev::init(void) {
+    // BlockDeviceDriverContainer : global container for kernel
+    BlockDeviceDriverContainer::get_self()->init(512);
+}
+
+/// @brief Register the block device driver
+/// @param driver Driver structure
+/// @param driver_name Name of the driver
+/// @return Return the id of driver
+max_t blockdev::register_driver(blockdev::block_device_driver *driver , const char *driver_name) {
+    debug::push_function("bdev::reg_drv");
+
+    BlockDeviceDriverContainer *driver_container = BlockDeviceDriverContainer::get_self();
+    max_t id = driver_container->register_driver(driver); // register driver to global container
+    if(id == INVALID) { debug::pop_function(); return INVALID; }
     
+    // assign new local device container
+    // Driver contains its devices
+    strcpy(driver->driver_info.driver_name , driver_name);
+    driver->prepare();
+    
+    debug::out::printf("Registered device driver, id : %d name : \"%s\"\n" , driver->driver_info.driver_id , driver->driver_info.driver_name);
+    debug::pop_function();
+    return driver->driver_info.driver_id;
+}
+
+///@brief some bridge-like functions(just basic stuff)
+
+blockdev::block_device_driver *blockdev::search_driver(const char *driver_name) { return BlockDeviceDriverContainer::get_self()->search_by_name(driver_name); }
+blockdev::block_device_driver *blockdev::search_driver(max_t driver_id) { return BlockDeviceDriverContainer::get_self()->get_object(driver_id); }
+
+max_t blockdev::discard_driver(const char *driver_name) { return BlockDeviceDriverContainer::get_self()->discard_object(BlockDeviceDriverContainer::get_self()->search_by_name(driver_name)); }
+max_t blockdev::discard_driver(max_t driver_id) { return BlockDeviceDriverContainer::get_self()->discard_object(BlockDeviceDriverContainer::get_self()->get_object(driver_id)); }
+
+/// @brief Registeres device to driver (kernel)
+/// @param driver Target driver
+/// @param device Device to be registered
+/// @return Return the id of the device
+max_t blockdev::register_device(blockdev::block_device_driver *driver , blockdev::block_device *device) {
+    debug::push_function("blockdev::reg_dev");
+    BlockDeviceDriverContainer *driver_container = BlockDeviceDriverContainer::get_self();
+    
+    device->id = driver->device_container->register_object(device);
+    if(device->id == INVALID) {
+        return false;
+    }
+    device->device_driver = driver;
+    return device->id;
+}
+
+/// @brief Other form of register_device
+
+max_t blockdev::register_device(const char *driver_name , blockdev::block_device *device) { return blockdev::register_device(BlockDeviceDriverContainer::get_self()->search_by_name(driver_name) , device); }
+max_t blockdev::register_device(max_t driver_id , blockdev::block_device *device) { return blockdev::register_device(BlockDeviceDriverContainer::get_self()->get_object(driver_id) , device); }
+
+blockdev::block_device *blockdev::search_device(max_t driver_id , max_t device_id) { return search_driver(driver_id)->device_container->get_object(device_id); }
+blockdev::block_device *blockdev::search_device(blockdev::block_device_driver *driver , max_t device_id) {  return search_device(driver->driver_info.driver_id , device_id); }
+blockdev::block_device *blockdev::search_device(const char *driver_name , max_t device_id) { return blockdev::search_device(BlockDeviceDriverContainer::get_self()->search_by_name(driver_name) , device_id); }
+
+bool blockdev::discard_device(blockdev::block_device *device) { return device->device_driver->device_container->discard_object(device); }
+
+/// @brief Create empty device with essential informations
+/// @param driver driver for device
+/// @param storage_type type of storage
+/// @return new empty device
+blockdev::block_device *blockdev::create_empty_device(blockdev::block_device_driver *driver) {
+    block_device *device = (block_device *)memory::pmem_alloc(sizeof(block_device));
+    device->device_driver = driver;
+    return device;
+}
+
+template <typename T> inline T *alloc_by_cnt(int cnt) { return (T *)memory::pmem_alloc(cnt*sizeof(T)); } 
+
+void blockdev::designate_resources_count(blockdev::block_device *device , int io_port_count , int interrupt_count , int flags_count , int etc_res_count) {
+    if(io_port_count > 0) {
+        device->resource.io_port_count = io_port_count;
+        device->resource.io_ports = alloc_by_cnt<io_port>(io_port_count);
+    }
+    if(flags_count > 0) {
+        device->resource.flags_count = flags_count;
+        device->resource.flags = alloc_by_cnt<resource_flag_t>(flags_count);
+    }
+    if(etc_res_count > 0) {
+        device->resource.etc_resources_count = etc_res_count;
+        device->resource.etc_resources = alloc_by_cnt<etc_resource_t>(etc_res_count);
+    }
 }

558581c89a70d0843f3901ba6c31b789e867d913
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-02-15 02:05:36 +0900

    Changed variable namespaces

--- kernel/Headers/drivers/partition_driver.hpp
+++ kernel/Headers/drivers/partition_driver.hpp
@@ -4,16 +4,17 @@
 #include <interface_type.hpp>
 #include <object_manager.hpp>
 #include <linked_list.hpp>
-#include <drivers/storage_device_driver.hpp>
+#include <drivers/block_device_driver.hpp>
+#include <drivers/storage_system.hpp>
 
-namespace storagedev {
+namespace storage_system {
     struct PartitionDriver {
-        virtual bool identify(storagedev::storage_device *device) = 0;
-	    virtual int get_partitions_count(storagedev::storage_device *device) = 0;
-    	virtual int get_partitions_list(storagedev::storage_device *device , DataLinkedList<storagedev::partition_info> &partition_info_list) = 0;
-    	virtual bool create_partition(storagedev::storage_device *device , storagedev::partition_info partition) = 0;
-        virtual bool remove_partition(storagedev::storage_device *device , storagedev::partition_info partition) = 0;
-        virtual bool modify_partition(storagedev::storage_device *device , storagedev::partition_info old_partition , storagedev::partition_info new_partition_info) = 0;
+        virtual bool identify(blockdev::block_device *device) = 0;
+	    virtual int get_partitions_count(blockdev::block_device *device) = 0;
+    	virtual int get_partitions_list(blockdev::block_device *device , DataLinkedList<blockdev::partition_info_t> &partition_info_list) = 0;
+    	virtual bool create_partition(blockdev::block_device *device , blockdev::partition_info_t partition) = 0;
+        virtual bool remove_partition(blockdev::block_device *device , blockdev::partition_info_t partition) = 0;
+        virtual bool modify_partition(blockdev::block_device *device , blockdev::partition_info_t old_partition , blockdev::partition_info_t new_partition_info) = 0;
 
         const char *driver_name;
     };
@@ -21,12 +22,12 @@ namespace storagedev {
     struct PartitionDriverContainer : ObjectManager<PartitionDriver> {
         SINGLETON_PATTERN_PMEM(PartitionDriverContainer);
 
-        friend max_t identify_partition_driver(storagedev::storage_device *device);
+        friend max_t identify_partition_driver(blockdev::block_device *device);
     };
 
-    max_t register_partition_driver(storagedev::PartitionDriver *partition_driver);
-    max_t identify_partition_driver(storagedev::storage_device *device);
-    PartitionDriver *get_partition_identifier(storagedev::storage_device *device);
+    max_t register_partition_driver(PartitionDriver *partition_driver);
+    max_t identify_partition_driver(blockdev::block_device *device);
+    PartitionDriver *get_partition_identifier(blockdev::block_device *device);
 }
 
 #endif
--- kernel/Sources/drivers/partition_driver.cpp
+++ kernel/Sources/drivers/partition_driver.cpp
@@ -1,6 +1,6 @@
 #include <drivers/partition_driver.hpp>
 
-max_t storagedev::register_partition_driver(storagedev::PartitionDriver *partition_driver) {
+max_t storage_system::register_partition_driver(storage_system::PartitionDriver *partition_driver) {
     PartitionDriverContainer *partitiondrv_container = PartitionDriverContainer::get_self();
     return partitiondrv_container->register_object(partition_driver);
 }
@@ -8,21 +8,16 @@ max_t storagedev::register_partition_driver(storagedev::PartitionDriver *partiti
 /// @brief Identify what partition driver block device uese
 /// @param device pointer of the block device
 /// @return id of using partition driver
-max_t storagedev::identify_partition_driver(storagedev::storage_device *device) {
+max_t storage_system::identify_partition_driver(blockdev::block_device *device) {
     PartitionDriverContainer *partitiondrv_container = PartitionDriverContainer::get_self();
     for(max_t id = 0; id < partitiondrv_container->max_count; id++) {
         if(partitiondrv_container->object_container[id].object->identify(device) == true) {
-            device->partition_driver_id = id;
             return id;
         }
     }
-    device->partition_driver_id = INVALID;
     return INVALID;
 }
 
-storagedev::PartitionDriver *storagedev::get_partition_identifier(storagedev::storage_device *device) {
-    if(device->partition_driver_id == INVALID) {
-        return 0x00;
-    }
-    return PartitionDriverContainer::get_self()->get_object(device->partition_driver_id);
+storage_system::PartitionDriver *storage_system::get_partition_identifier(blockdev::block_device *device) {
+    return PartitionDriverContainer::get_self()->get_object(device->storage_info.partition_driver_id);
 }

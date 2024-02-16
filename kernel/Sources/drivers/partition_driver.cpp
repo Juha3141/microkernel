15ebcf40c1bbce1acbd23fbf05b108c9b738038b
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-02-16 13:38:02 +0900

    Update partition_driver.cpp
    Slight change

--- kernel/Sources/drivers/partition_driver.cpp
+++ kernel/Sources/drivers/partition_driver.cpp
@@ -12,12 +12,18 @@ max_t storage_system::identify_partition_driver(blockdev::block_device *device) 
     PartitionDriverContainer *partitiondrv_container = PartitionDriverContainer::get_self();
     for(max_t id = 0; id < partitiondrv_container->max_count; id++) {
         if(partitiondrv_container->object_container[id].object->identify(device) == true) {
+            device->storage_info.partition_driver_id = id;
             return id;
         }
     }
+    device->storage_info.partition_driver_id = INVALID;
     return INVALID;
 }
 
 storage_system::PartitionDriver *storage_system::get_partition_identifier(blockdev::block_device *device) {
     return PartitionDriverContainer::get_self()->get_object(device->storage_info.partition_driver_id);
+}
+
+storage_system::PartitionDriver *storage_system::get_partition_identifier(max_t identifier_id) {
+    return PartitionDriverContainer::get_self()->get_object(identifier_id);
 }

fc561d71a3b5cf14cacb459187d916e01b00c4f1
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-02-22 22:06:14 +0900

    Create device_driver.hpp
    Sorted some duplicated stuffs into one class

---
+++ kernel/Headers/drivers/device_driver.hpp
@@ -0,0 +1,40 @@
+#ifndef _GENERAL_DEVICE_DRIVER_HPP_
+#define _GENERAL_DEVICE_DRIVER_HPP_
+
+#include <interface_type.hpp>
+#include <object_manager.hpp>
+#include <linked_list.hpp>
+#include <interrupt.hpp>
+
+typedef max_t resource_flag_t;
+typedef max_t etc_resource_t;
+
+struct device_resources {
+    int io_port_count;
+    io_port *io_ports;
+
+    int interrupt_count;
+    interrupt::interrupt_info_t *interrupts;
+    
+    int flags_count;
+    resource_flag_t *flags;
+
+    int etc_resources_count;
+    etc_resource_t *etc_resources;
+};
+
+// Template : Device driver
+template <typename T> struct general_device {
+    max_t id;
+    T *device_driver;
+    device_resources resources;
+};
+
+// Template : Device container
+template <typename T> struct general_device_driver {
+    max_t driver_id;
+    T *device_container;
+    char driver_name[24];
+};
+
+#endif

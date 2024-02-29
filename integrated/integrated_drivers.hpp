6a65a4404861854aea43fe77512215ea5f1565ca
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-02-29 21:36:40 +0900

    Created folder that will contain integrated drivers

---
+++ integrated/integrated_drivers.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file integrated_drivers.hpp
+ * @author Ian Juha Cho (ianisnumber2027@gmail.com)
+ * @brief The header file that contains all the headers and lists of integrated drivers that will be used by the kernel.
+ * @date 2024-02-27
+ * 
+ * @copyright Copyright (c) 2024 Ian Juha Cho. 
+ * 
+ */
+
+#ifndef _INTEGRATED_DRIVERS_HPP_
+#define _INTEGRATED_DRIVERS_HPP_
+
+// headers here
+#include <ramdisk/ramdisk.hpp>
+
+// register_driver function here
+#define INTEGRATED_DRIVER_LIST { \
+    ramdisk_driver::register_driver , \
+}
+
+#endif
---
+++ kernel/include/kernel/integrated.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file integrated.hpp
+ * @author Ian Juha Cho (ianisnumber2027@gmail.com)
+ * @brief Contains the definition of function that registers basic kernel drivers. 
+ * @date 2024-02-29
+ * 
+ * @copyright Copyright (c) 2024 Ian Juha Cho.
+ * 
+ */
+
+#ifndef _INTEGRATED_HPP_
+#define _INTEGRATED_HPP_
+
+void register_basic_kernel_drivers(void);
+
+#endif

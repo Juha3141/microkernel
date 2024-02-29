68bd02d171ad13b96783f334501b69d7649fb0ba
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-02-29 21:36:23 +0900

    Created folder that will contain file systems

---
+++ filesystem/file_systems.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file file_systems.hpp
+ * @author Ian Juha Cho (ianisnumber2027@gmail.com)
+ * @brief The header file that contains all the headers and lists of file systems that will be used by the kernel. 
+ * @date 2024-02-27
+ * 
+ * @copyright Copyright (c) 2024 Ian Juha Cho. 
+ * 
+ */
+
+#ifndef _FILE_SYSTEMS_HPP_
+#define _FILE_SYSTEMS_HPP_
+
+// headers here
+#include <partitions/mbr.hpp>
+#include <partitions/gpt.hpp>
+
+// register_driver function here
+#define FILESYSTEM_DRIVER_LIST { \
+    MBRPartitionDriver::register_driver , \
+    GPTPartitionDriver::register_driver , \
+}
+
+#endif

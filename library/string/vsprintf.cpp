d00de8d9af46293ba7363c011ba0fe231313133a
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-03-06 21:25:44 +0900

    Added sprintf that was missing

--- library/string/vsprintf.cpp
+++ library/string/vsprintf.cpp
@@ -223,6 +223,14 @@ int vsprintf(char *buf , const char *fmt , va_list ap) {
     return buf_i;
 }
 
+int sprintf(char *buf , const char *fmt , ...) {
+    va_list ap;
+    va_start(ap , fmt);
+    int ret = vsprintf(buf , fmt , ap);
+    va_end(ap);
+    return ret;
+}
+
 template <typename T> char* itoa_variation(T value, char* result, int base , bool lowercase) {
 	// check that the base if valid
     char sample_str_low[] =  "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz";

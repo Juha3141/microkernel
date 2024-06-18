f99eb2bd84eb860eae5fed6e1549dac24fd00b5d
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-06-18 15:27:22 +0900

    Update essentials.hpp

--- kernel/include/kernel/essentials.hpp
+++ kernel/include/kernel/essentials.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file essentials.hpp
+ * @author Ian Juha Cho (ianisnumber2027@gmail.com)
+ * @brief A header that includes some essential files
+ * @date 2024-06-12
+ * 
+ * @copyright Copyright (c) 2024 Ian Juha Cho.
+ * 
+ */
+
 #ifndef _KERNEL_ESSENTIALS_HPP_
 #define _KERNEL_ESSENTIALS_HPP_
 

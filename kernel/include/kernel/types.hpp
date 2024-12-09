6b0d7b133195f7750d0076a313fca635a16928f7
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-12-09 23:30:51 +0900

    Update types.hpp

--- kernel/include/kernel/types.hpp
+++ kernel/include/kernel/types.hpp
@@ -33,25 +33,28 @@ static type *SINGLETON_FUNCTION(void) {\
 #define GLOBAL_OBJECT(type) type::SINGLETON_FUNCTION() 
 
 // Maximum calculatable data size, corresponds to architecture(16-bit,32-bit, ...)
-typedef unsigned long max_t;
-typedef signed long max_s_t;
+typedef unsigned long  max_t;
+typedef signed long    max_s_t;
 
-typedef unsigned long qword;
-typedef signed long qword_s;
+typedef unsigned long  qword;
+typedef signed long    qword_s;
 
-typedef unsigned int dword;
-typedef signed int dword_s;
+typedef unsigned int   dword;
+typedef signed int     dword_s;
 typedef unsigned short word;
-typedef signed short word_s;
-typedef unsigned char byte;
-typedef signed char byte_s;
+typedef signed short   word_s;
+typedef unsigned char  byte;
+typedef signed char    byte_s;
 
 typedef max_t ptr_t;
 typedef void vptr_t;
 
 typedef unsigned int size_t;
 
-// to-do!!
-typedef void (*interrupt_handler_t)(void);
+typedef void (*interrupt_handler_t)(struct Registers *regs);
+
+#define ARCHDEP // indicates that the function is architecture-dependent
+
+#define offsetof(s, m) ((max_t)&(((s *)0)->m))
 
 #endif

540b0dca1170cebef0d0ebdf7fe65d875bd21a67
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-12-09 23:23:59 +0900

    Update pic.cpp
    Minor changes

--- arch/x86_64/src/pic.cpp
+++ arch/x86_64/src/pic.cpp
@@ -2,23 +2,35 @@
 #include <kernel/io_port.hpp>
 #include <kernel/debug.hpp>
 
+void io_wait(void) {
+    io_write_byte(0x80 , 0x00);
+}
+
 void x86_64::pic::init(void) {
     // remap PIC
 
     // For Master : 
     io_write_byte(PIC_MASTER_COMMAND , PIC_ICW1_INIT|PIC_ICW1_ICW4_P);
+    io_wait();
     io_write_byte(PIC_MASTER_DATA , 32); // start vector of interrupt (Master)
+    io_wait();
     io_write_byte(PIC_MASTER_DATA , 0b00000100); // IRQ2 : Where Slave is connected
+    io_wait();
     io_write_byte(PIC_MASTER_DATA , PIC_ICW4_8086);
+    io_wait();
 
     // For Slave : 
     io_write_byte(PIC_SLAVE_COMMAND , PIC_ICW1_INIT|PIC_ICW1_ICW4_P);
+    io_wait();
     io_write_byte(PIC_SLAVE_DATA , 32+8); // start vector of interrupt (Slave)
+    io_wait();
     io_write_byte(PIC_SLAVE_DATA , 0b00000010); // ??
+    io_wait();
     io_write_byte(PIC_SLAVE_DATA , PIC_ICW4_8086);
+    io_wait();
 
-    io_write_byte(PIC_MASTER_DATA , 0x00);
-    io_write_byte(PIC_SLAVE_DATA , 0x00);
+    io_write_byte(PIC_MASTER_DATA , 0xff);
+    io_write_byte(PIC_SLAVE_DATA , 0xff);
 }
 
 void x86_64::pic::disable(void) {
@@ -27,8 +39,8 @@ void x86_64::pic::disable(void) {
 }
 
 void x86_64::pic::irq_mask(int irq) {
-    word port = (irq > 8) ? PIC_SLAVE_DATA : PIC_MASTER_DATA;
-    irq = (irq > 8) ? irq-8 : irq;
+    word port = (irq >= 8) ? PIC_SLAVE_DATA : PIC_MASTER_DATA;
+    irq       = (irq >= 8) ? irq-8          : irq;
     byte value = io_read_byte(port);
     
     value |= (1 << irq);
@@ -37,8 +49,8 @@ void x86_64::pic::irq_mask(int irq) {
 }
 
 void x86_64::pic::irq_unmask(int irq) {
-    word port = (irq > 8) ? PIC_SLAVE_DATA : PIC_MASTER_DATA;
-    irq = (irq > 8) ? irq-8 : irq;
+    word port = (irq >= 8) ? PIC_SLAVE_DATA : PIC_MASTER_DATA;
+    irq       = (irq >= 8) ? irq-8          : irq;
     byte value = io_read_byte(port);
     
     value &= ~(1 << irq);

cb0055325ca64435fef8e934a69e984f6b49f47e
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-01-06 19:02:41 +0900

    Added automatic registration system

---
+++ kernel/src/driver/device_driver.cpp
@@ -0,0 +1,11 @@
+#include <kernel/driver/device_driver.hpp>
+
+extern qword __drivers_init_start__;
+extern qword __drivers_init_end__;
+
+void register_kernel_device_drivers(void) {
+    for(qword func_ptr_ptr = (qword)&__drivers_init_start__; func_ptr_ptr < (qword)&__drivers_init_end__; func_ptr_ptr += sizeof(device_driver_init_func_ptr_t)) {
+        qword func_ptr = *((qword *)func_ptr_ptr);
+        ((void(*)(void))func_ptr)();
+    }
+}

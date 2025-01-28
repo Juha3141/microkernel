7ad3e871510587638e2045868b8846f810cd41b1
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-01-28 12:36:13 +0900

    Moved compiler options to arch_configurations.mk

---
+++ arch/x86_64/arch_configurations.mk
@@ -0,0 +1,15 @@
+AS = as
+CC = g++
+LD = ld
+OBJDUMP = objdump
+OBJCOPY = objcopy
+
+KERNEL_COMPILER = x86_64-elf
+KERNEL_CCOPTIONS = -m64 -g -ffreestanding -nostdlib \
+-mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 \
+-fpack-struct=1 -masm=intel \
+-Werror=return-type -fno-stack-protector \
+-fno-use-cxa-atexit -fno-threadsafe-statics \
+-fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings
+KERNEL_LDOPTIONS = -m elf_x86_64
+KERNEL_ASOPTIONS = 

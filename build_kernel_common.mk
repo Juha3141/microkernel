35f8c1bc9d88cae4adde73510b3fcbd1f146b85d
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-01-28 12:37:42 +0900

    Update build_kernel_common.mk

--- build_kernel_common.mk
+++ build_kernel_common.mk
@@ -3,8 +3,11 @@
 # SEARCH_LOCATION (only for kernel option)
 # INCLUDEPATHS 
 
-include $(ROOTDIR)/global_variables.mk
 include $(ROOTDIR)/configurations.mk
+include $(ROOTDIR)/global_variables.mk
+include $(ROOTDIR)/$(ARCH_CONFIGURATION_MAKEFILE)
+
+ASM_EXTENSION = .S
 
 BINARYFOLDER = $(ROOTDIR)/$(ROOTBINARYFOLDER)
 MAINBINARYFOLDER = $(BINARYFOLDER)/$(CURRENTFOLDER)
@@ -13,13 +16,13 @@ ifeq ($(COMPILE_TYPE),kernel)
 # COMPILE_TYPE = kernel
 SUBDIRECTORIES = $(subst $(COMMON_SRCFOLDER),$(MAINBINARYFOLDER),$(sort $(dir $(wildcard $(COMMON_SRCFOLDER)/*/))))
 MAINTARGETS = $(subst .cpp,.obj,$(wildcard $(SEARCH_LOCATION)/*.cpp) $(wildcard $(SEARCH_LOCATION)/*/*.cpp))
-ASMTARGETS = $(subst .asm,.aobj,$(wildcard $(SEARCH_LOCATION)/*.asm))
+ASMTARGETS = $(subst $(ASM_EXTENSION),.aobj,$(wildcard $(SEARCH_LOCATION)/*$(ASM_EXTENSION)))
 
 else
 # COMPILE_TYPE = driver
 SUBDIRECTORIES = $(sort $(dir $(wildcard ./*/)))
 MAINTARGETS = $(foreach dir,$(SUBDIRECTORIES),$(if $(wildcard $(dir)*.cpp),$(subst .cpp,.obj,$(wildcard $(dir)*.cpp))))
-ASMTARGETS = $(foreach dir,$(SUBDIRECTORIES),$(if $(wildcard $(dir)*.asm),$(subst .asm,.aobj,$(wildcard $(dir)*.asm))))
+ASMTARGETS = $(foreach dir,$(SUBDIRECTORIES),$(if $(wildcard $(dir)*$(ASM_EXTENSION)),$(subst .$(ASM_EXTENSION),.aobj,$(wildcard $(dir)*$(ASM_EXTENSION)))))
 
 endif
 
@@ -38,12 +41,12 @@ clean:
 
 %.obj: %.cpp
 ifeq ($(COMPILE_TYPE),kernel)
-	$(KERNEL_CC) -m64 -c $< -o $(subst $(SEARCH_LOCATION),$(MAINBINARYFOLDER),$@) $(KERNEL_CCOPTIONS) $(INCLUDEPATHS)
+	$(KERNEL_CC) -c $< -o $(subst $(SEARCH_LOCATION),$(MAINBINARYFOLDER),$@) $(KERNEL_CCOPTIONS) $(INCLUDEPATHS)
 else
-	$(KERNEL_CC) -m64 -c $< -o $(MAINBINARYFOLDER)/$@ $(KERNEL_CCOPTIONS) $(INCLUDEPATHS)
+	$(KERNEL_CC) -c $< -o $(MAINBINARYFOLDER)/$@ $(KERNEL_CCOPTIONS) $(INCLUDEPATHS)
 endif
 
-%.aobj: %.asm
-	$(KERNEL_AS) $< -f elf64 -o $(subst $(SEARCH_LOCATION),$(MAINBINARYFOLDER),$@)
+%.aobj: %$(ASM_EXTENSION)
+	$(KERNEL_AS) $< $(KERNEL_ASOPTIONS) -o $(subst $(SEARCH_LOCATION),$(MAINBINARYFOLDER),$@)
 
 .PHONY: clean all 

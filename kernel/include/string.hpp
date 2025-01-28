06e04d9d04dc9ba82869ddfb1bdbedb156634308
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-01-28 13:21:49 +0900

    Added memmove() function

--- kernel/include/string.hpp
+++ kernel/include/string.hpp
@@ -7,8 +7,9 @@
 #define MIN(X , Y) ((X) >= (Y) ? (Y) : (X))
 #define MAX(X , Y) ((X) >= (Y) ? (X) : (Y))
 
-void *memset(void *s , int c , size_t n);
+void *memset(void *dest , int c , size_t n);
 void *memcpy(void *dest , const void *src , size_t n);
+void *memmove(void *dest , const void *src , size_t n);
 int memcmp(const void *s1 , const void *s2 , size_t n);
 
 size_t strlen(const char *s);

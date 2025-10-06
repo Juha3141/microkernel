8203df023d471822bf27670f8f1a5c619972c95f
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-10-06 11:14:18 -0500

    Update types.hpp
    Added uint/int types, fixed some bugs in SINGLETON_FUNCTION macros

--- kernel/include/kernel/types.hpp
+++ kernel/include/kernel/types.hpp
@@ -14,19 +14,26 @@
 #include <loader/loader_argument.hpp>
 #include <kernel/configurations.hpp>
 
+namespace debug{ namespace out { void printf(const char *string , ...); }}
+
 #define SINGLETON_FUNCTION get_self
 
 #define SINGLETON_PATTERN_KSTRUCT(type) \
 static type *SINGLETON_FUNCTION(void) {\
-    static type *p = 0x00;\
-    if(!p) p = (type *)memory::kstruct_alloc(sizeof(type));\
+    /* for whatever reason, if you initialize the static variable with zero, the variable somehow gets located in a weird address :/ */ \
+    static type *p;\
+    if(p == 0x00) { \
+        p = (type *)memory::kstruct_alloc(sizeof(type));\
+    }\
     return p;\
 }
 
 #define SINGLETON_PATTERN_PMEM(type) \
 static type *SINGLETON_FUNCTION(void) {\
-    static type *p = 0x00;\
-    if(!p) p = (type *)memory::pmem_alloc(sizeof(type));\
+    static type *p;\
+    if(p == 0x00) { \
+        p = (type *)memory::pmem_alloc(sizeof(type));\
+    } \
     return p;\
 }
 
@@ -37,14 +44,22 @@ typedef unsigned long  max_t;
 typedef signed long    max_s_t;
 
 typedef unsigned long  qword;
+typedef unsigned long  uint64_t;
 typedef signed long    qword_s;
+typedef signed long    int64_t;
 
 typedef unsigned int   dword;
+typedef unsigned int   uint32_t;
 typedef signed int     dword_s;
+typedef signed int     int32_t;
 typedef unsigned short word;
+typedef unsigned short uint16_t;
 typedef signed short   word_s;
+typedef signed short   int16_t;
 typedef unsigned char  byte;
+typedef unsigned char  uint8_t;
 typedef signed char    byte_s;
+typedef signed char    int8_t;
 
 typedef max_t ptr_t;
 

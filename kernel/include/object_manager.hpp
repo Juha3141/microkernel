f2c037a12249589e6ee0250f715381425a26f322
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-10-24 22:51:02 -0500

    Merged ObjectManager and DataManager into FixedArray
    FixedArray works the same with both ObjectManager and DataManager, with some enhancements

--- kernel/include/object_manager.hpp
+++ kernel/include/object_manager.hpp
@@ -4,131 +4,118 @@
 #include <kernel/essentials.hpp>
 #include <string.hpp>
 #include <kernel/mem/kmem_manager.hpp>
+#include <kernel/debug.hpp>
 
 #include <kernel/debug.hpp>
 
-template <typename T> class DataManager {
+template <typename T> class FixedArray {
+public:
+    FixedArray() = default;
+    void init(T *arr , max_t sz , max_t m_cnt , memory::memory_allocator_func_t f_alloc=memory::pmem_alloc , memory::memory_deallocator_func_t f_free=memory::pmem_free) {
+        count = sz;
+        max_count = m_cnt;
+        m_alloc = f_alloc;
+        m_free = f_free;
+        container = (FixedArray::data_container_s *)m_alloc(max_count*sizeof(FixedArray::data_container_s) , 0);
+        for(max_t i = 0; i < sz; i++) {
+            container[i].occupied = true;
+            (*container[i]) = arr[i];
+        }
+    }
+    void init(max_t m_cnt , memory::memory_allocator_func_t f_alloc=memory::pmem_alloc , memory::memory_deallocator_func_t f_free=memory::pmem_free) {
+        count = 0;
+        max_count = m_cnt;
+        m_alloc = f_alloc;
+        m_free = f_free;
+
+        container = (FixedArray::data_container_s *)m_alloc(max_count*sizeof(FixedArray::data_container_s) , 0);
+        for(max_t i = 0; i < max_count; i++) {
+            container[i].occupied = false;
+        }
+    }
+    max_t add_empty_space() {
+        if(container == 0x00) {
+            return INVALID;
+        }
+        if(count >= max_count) return INVALID;
+        max_t i = 0;
+        for(; i < max_count; i++) {
+            if(container[i].occupied == false) break;
+        }
+        if(i >= max_count) return INVALID; // all object is occupied
+        container[i].occupied = true;
+        count++;
+        return i;
+    }
+    max_t add(const T& data) {
+        max_t id = add_empty_space();
+        if(id == INVALID) return INVALID;
+
+        (*container[id]) = data;
+        return id;
+    }
+    bool discard(T matching_data) {
+        for(max_t i = 0; i < max_count; i++) {
+            if((*container[i]) == matching_data) {
+                return discard_space(i);
+            }
+        }
+        return false;
+    }
+    bool discard_space(max_t id) {
+        if(container[id].occupied != true) return false;
+
+        container[id].occupied = false;
+        count--;
+        return true;
+    }
+    template <typename T2> max_t search(bool (*check)(T& data , T2 sample_data) , T2 sample_data) {
+        for(max_t i = 0; i < max_count; i++) {
+            if(check(*container[i] , sample_data) == true) {
+                return i;
+            }
+        }
+        return INVALID;
+    }
+    T &get(max_t id) const {
+        if(id >= max_count) return *((T *)nullptr);
+        
+        return *container[id];
+    }
+    T &operator[](max_t id) const { return *container[id]; }
+
+    max_t get_max_size() const { return max_count; }
+    max_t size() const { return count; }
+
+    /// @brief Deallocate the data container. Warning: To prevent memory leak, you must de-allocate all the objects in the container!
+    void deallocate_this() {
+        // if m_free is nullptr, this array can't be de-allocated
+        if(m_free == nullptr) return;
+
+        m_free(container);
+        max_count = 0;
+        count = 0;
+    }
+protected:
+    class data_container_s {
     public:
-        void init(int max_data_count) {
-            int i;
-            count = 0;
-            max_count = max_data_count;
-            data_container = (struct DataManager::data_container_s *)memory::pmem_alloc(max_count*sizeof(struct DataManager::data_container_s));
-            for(i = 0; i < max_count; i++) {
-                data_container[i].occupied = false;
-                memset(&data_container[i].data , 0 , sizeof(T));
-            }
-        }
-        
-        max_t register_space(void) {
-            max_t i;
-            if(data_container == 0x00) {
-                // debug::out::printf(DEBUG_WARNING , "Error : ObjectContainer yet not initialized\n");
-                return INVALID;
-            }
-            if(count >= max_count) return INVALID;
-            for(i = 0; i < max_count; i++) {
-                if(data_container[i].occupied == false) {
-                    break;
-                }
-            }
-            if(i >= max_count) return INVALID; // all object is occupied
-            data_container[i].occupied = true;
-            count++;
-            return i;
-        }
-        bool discard_space(max_t id) {
-            if(data_container[id].occupied == false) return false;
-            data_container[id].occupied = true;
-            memset(&data_container[id].data , 0 , sizeof(T));
-            count--;
-            return true;
-        }
-        template <typename T2> max_t search(bool (*check)(T &data , T2 sample_data) , T2 sample_data) {
-            for(max_t i = 0; i < max_count; i++) {
-                if(check(data_container[i].data , sample_data) == true) {
-                    return i;
-                }
-            }
-            return INVALID;
-        }
-        
-        // some inline function
-        inline T *get_data(max_t id) { return &data_container[id].data; }
+        bool occupied;
 
-        max_t max_count = 0;
-        max_t count = 0;
-    protected:
-        struct data_container_s {
-            bool occupied;
+        T& operator*(void) { return data; }
+        data_container_s &operator=(const T &d) {
+            data = d;
+            return *this;
+        }
+    private:
+        T data;
+    }*container = 0x00;
 
-            T data;
-        }*data_container = 0x00;
-};
+private:
+    memory::memory_allocator_func_t   m_alloc = memory::pmem_alloc;
+    memory::memory_deallocator_func_t m_free  = memory::pmem_free;
 
-// Manages [Pointer of] some object
-template <typename T> class ObjectManager {
-    public:
-        void init(int max_obj_count) {
-            int i;
-            count = 0;
-            max_count = max_obj_count;
-            object_container = (struct ObjectManager::object_container_s *)memory::pmem_alloc(sizeof(struct ObjectManager::object_container_s)*max_count);
-            for(i = 0; i < max_count; i++) {
-                object_container[i].occupied = false;
-                object_container[i].object = 0x00;
-            }
-        }
-        max_t register_object(T *object) { // returns ID
-            max_t i;
-            if(object_container == 0x00) {
-                // debug::out::printf(DEBUG_WARNING , "Error : ObjectContainer yet not initialized\n");
-                return INVALID;
-            }
-            if(count >= max_count) return INVALID;
-            for(i = 0; i < max_count; i++) {
-                if(object_container[i].occupied == false) {
-                    break;
-                }
-            }
-            if(i >= max_count) return INVALID;
-            object_container[i].occupied = true;
-            object_container[i].object = object;
-            count++;
-            return i;
-        }
-        max_t discard_object(T *object) {
-            max_t i;
-            for(i = 0; i < max_count; i++) {
-                if(object_container[i].object == object) {
-                    count--;
-                    object_container[i].occupied = false;
-                    return i;
-                }
-            }
-            return INVALID;
-        }
-        T *get_object(max_t id) {
-            if((id >= max_count)||(object_container[id].occupied == false)) return 0x00;
-            return object_container[id].object;
-        }
-        
-        template <typename T2> max_t search(bool (*check)(T *data , T2 sample_data) , T2 sample_data) {
-            for(max_t i = 0; i < max_count; i++) {
-                if(check(object_container[i].object , sample_data) == true) {
-                    return i;
-                }
-            }
-            return INVALID;
-        }
-        max_t max_count = 0;
-        max_t count = 0;
-    protected:
-        struct object_container_s {
-            bool occupied;
-
-            T *object;
-        }*object_container = 0x00;
+    max_t max_count = 0;
+    max_t count = 0;
 };
 
 #endif

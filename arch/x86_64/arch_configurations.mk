a96adbd0bc759e0265665a98f894bf0ab521b62f
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-11-06 11:49:11 -0600

    Changed compile options

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -1,15 +1,18 @@
-AS = as
+AS = nasm
 CC = g++
 LD = ld
 OBJDUMP = objdump
 OBJCOPY = objcopy
 
 KERNEL_COMPILER = x86_64-elf
-KERNEL_CCOPTIONS = -m64 -g -ffreestanding -nostdlib -std=c++20 \
+
+#--target=x86_64-elf -m64 -g -ffreestanding -nostdlib -std=c++20 \
 -mcmodel=large -mno-mmx -mno-sse -mno-sse2 \
 -fpack-struct=1 -masm=intel \
 -Werror=return-type -fno-stack-protector \
 -fno-use-cxa-atexit -fno-threadsafe-statics \
--fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings
-KERNEL_LDOPTIONS = -m elf_x86_64
-KERNEL_ASOPTIONS = 
+-fno-rtti -fno-exceptions -Wno-write-strings
+
+KERNEL_CCOPTIONS = --target=x86_64-pc-none-elf -march=x86-64 -ffreestanding -fno-builtin -nostdlib -nostdinc++ -fpack-struct=1 -masm=intel -std=c++20 -Werror=return-type -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-stack-protector -fno-threadsafe-statics -O0 -fPIC -fPIE -g 
+KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding -mcmodel=large
+KERNEL_ASOPTIONS = -f elf64

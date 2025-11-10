fe59e91dc6c0def99453c610d93c7b076aaec4b8
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-11-10 17:57:00 -0600

    Update global_variables.mk
    Changed stuffs on configuration system

--- global_variables.mk
+++ global_variables.mk
@@ -24,14 +24,12 @@ KERNEL_CC = clang++
 KERNEL_LD = clang++
 KERNEL_OBJDUMP = $(OBJDUMP)
 KERNEL_OBJCOPY = $(OBJCOPY)
-KERNEL_LINKERSCRIPT = kernel_linker.ld
+KERNEL_LINKERSCRIPT = kernel_linker.ldX
 
 PWD = $(shell pwd)
 ARCH_CONFIGURATION_FILE_LOC     = $(ARCHFOLDER)/$(ARCH)
 ARCH_CONFIGURATION_FILE         = arch_configurations.hpp
 
-define convert_hpp_to_ld
-	echo \#include \"$(notdir $(1))\" > $(dir $(1))dummy.ld
-	$(KERNEL_CC) -E -P -xc -DLINKER_SCRIPT $(dir $(1))dummy.ld > $(subst .hpp,.ld,$(1))
-	rm $(dir $(1))dummy.ld
+define convert_ldx_to_ld
+	$(KERNEL_CC) -E -P -xc $(1) $(3) > $(2)
 endef

1a75bd6998cc6a30bddd8c44664743534efbced5
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-01-25 22:28:00 -0600

    Update arch_configurations.mk
    To-do : integrate conf file with makefile configuration

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -13,6 +13,14 @@ KERNEL_COMPILER = x86_64-elf
 -fno-use-cxa-atexit -fno-threadsafe-statics \
 -fno-rtti -fno-exceptions -Wno-write-strings
 
-KERNEL_CCOPTIONS = --target=x86_64-pc-none-elf -march=x86-64 -ffreestanding -fno-builtin -nostdlib -nostdinc++ -fpack-struct=1 -masm=intel -std=c++20 -Werror=return-type -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-stack-protector -fno-threadsafe-statics -O0 -fPIC -fPIE -g 
-KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding -mcmodel=large
+# To-do : integrate conf file with makefile configurations
+KASAN_OFFSET  = 0xffaa000000000
+KASAN_OPTIONS = # -fsanitize=kernel-address \
+	            -mllvm -asan-stack=1 \
+				-mllvm -asan-globals=1 \
+	            -mllvm -asan-instrumentation-with-call-threshold=0 \
+				-mllvm -asan-mapping-offset=$(KASAN_OFFSET)
+
+KERNEL_CCOPTIONS = --target=x86_64-pc-linux-gnu -march=x86-64 -ffreestanding -fPIC -fno-builtin -nostdlib -nostdinc++ -fpack-struct=1 -masm=intel -std=c++20 -Werror=return-type -Wno-incompatible-library-redeclaration -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-stack-protector -fno-threadsafe-statics -O0 -g $(KASAN_OPTIONS)
+KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding -mcmodel=large 
 KERNEL_ASOPTIONS = -f elf64

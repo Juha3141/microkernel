9ed641bcf3da3d7ca6e33e7f31888876e1d93355
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-03-26 01:09:26 -0500

    Added KERNEL_SETUP_CCOPTIONS, changed some options

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -1,4 +1,4 @@
-AS = nasm
+AS = as
 CC = g++
 LD = ld
 OBJDUMP = objdump
@@ -20,7 +20,10 @@ KASAN_OPTIONS = # -fsanitize=kernel-address \
 				-mllvm -asan-globals=1 \
 	            -mllvm -asan-instrumentation-with-call-threshold=0 \
 				-mllvm -asan-mapping-offset=$(KASAN_OFFSET)
+COMMON_CCOPTIONS = --target=x86_64-pc-linux-gnu -march=x86-64 -ffreestanding -fno-builtin -nostdlib -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -nostdinc++ -fpack-struct=1 -masm=intel -std=c++20 -Werror=return-type -Wno-incompatible-library-redeclaration -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -O0 -g -mcmodel=large
 
-KERNEL_CCOPTIONS = --target=x86_64-pc-linux-gnu -march=x86-64 -ffreestanding -fPIC -fno-builtin -nostdlib -nostdinc++ -fpack-struct=1 -masm=intel -std=c++20 -Werror=return-type -Wno-incompatible-library-redeclaration -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-stack-protector -fno-threadsafe-statics -O0 -g $(KASAN_OPTIONS)
-KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding -mcmodel=large 
-KERNEL_ASOPTIONS = -f elf64
+KERNEL_SETUP_CCOPTIONS =  -fno-stack-protector -fno-pic -fno-pie
+KERNEL_CCOPTIONS       =  -fPIC $(KASAN_OPTIONS)
+
+KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding
+KERNEL_ASOPTIONS = 

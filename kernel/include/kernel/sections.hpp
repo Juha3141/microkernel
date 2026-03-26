ffb72f01e9581d46ca68cf58aa4c17247e1449df
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-03-26 01:22:48 -0500

    Added kernel_setup_stage section

--- kernel/include/kernel/sections.hpp
+++ kernel/include/kernel/sections.hpp
@@ -1,7 +1,9 @@
 #ifndef _SECTION_HPP_
 #define _SECTION_HPP_
 
-#define __entry_function__ __attribute__ ((section(".entry")))
-#define __singleton_ptr__  __attribute__ ((section(".singleton_obj_ptrs")))
+#define __kernel_setup_text__ __attribute__ ((section(".kernel_setup_stage.text")))
+#define __kernel_setup_data__ __attribute__ ((section(".kernel_setup_stage.data")))
+#define __entry_function__        __attribute__ ((section(".entry")))
+#define __singleton_ptr__         __attribute__ ((section(".singleton_obj_ptrs")))
 
 #endif

35aa1d401f58b523449a2ba9a32526fb370297bf
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-04-04 22:58:04 -0500

    Finished the draft, needs some testing

--- page_algorithm_test.cpp
+++ page_algorithm_test.cpp
@@ -19,6 +19,7 @@ void map_pages(max_t linear_addr , max_t page_size , max_t page_count , max_t ph
 }
 
 max_t figure_out_page_mapping_alignment(max_t phys_addr_start , max_t phys_addr_end , max_t linear_addr_start) {
+    if(phys_addr_start == phys_addr_end) return 0;
     // default will be levels_of_page_size[0]
    constexpr max_t levels_of_page_size[] = {
         CONFIG_PAGE_SIZE , 
@@ -47,46 +48,45 @@ max_t figure_out_page_mapping_alignment(max_t phys_addr_start , max_t phys_addr_
     
     max_t phys_addr_offset   = 0;
     max_t linear_addr_offset = 0;
-    max_t larger_page_size = levels_of_page_size[page_size_idx];
+    max_t page_size = levels_of_page_size[page_size_idx];
+
+    max_t addr_size = phys_addr_end-phys_addr_start;
     
-    while(((phys_addr_start+phys_addr_offset)%larger_page_size != 0)
-       || ((linear_addr_start+linear_addr_offset)%larger_page_size != 0)) {
-        max_t phys_addr = phys_addr_start+phys_addr_offset;
-        max_t linear_addr = linear_addr_start+linear_addr_offset;
-        if(phys_addr%larger_page_size != 0) phys_addr_offset += smaller_page_size;
-        if(linear_addr%larger_page_size != 0) linear_addr_offset += smaller_page_size;
+    max_t aligned_phys_addr   = align_round_up(phys_addr_start , page_size);
+    max_t alignment_padding_phys = aligned_phys_addr-phys_addr_start;
+    max_t aligned_linear_addr = align_round_up(linear_addr_start , page_size);
+    max_t alignment_padding_linear = aligned_linear_addr-linear_addr_start;
+
+    max_t aligned_phys_end = align_round_down(phys_addr_end , page_size);
+    max_t alignment_padding_phys_end = phys_addr_end-aligned_phys_end;
+
+    max_t aligned_linear_end = align_round_down(linear_addr_start+addr_size , page_size);
+    max_t alignment_padding_linear_end = linear_addr_start+addr_size-aligned_linear_end;
+
+    if(alignment_padding_phys < alignment_padding_linear) {
+        alignment_padding_phys += page_size;
+        aligned_phys_addr      += page_size;
     }
-    map_pages(linear_addr_start , smaller_page_size , min(phys_addr_offset , linear_addr_offset)/smaller_page_size , phys_addr_start);
+    printf(" =========== lower unaligned area mapping ============ \n");
+    figure_out_page_mapping_alignment(phys_addr_start , phys_addr_start+alignment_padding_linear , linear_addr_start);
+    printf(" ========== lower unaligned area mapping end ========= \n");
 
-    if(smaller_page_size == larger_page_size) {
-        return;
-    }
-    
-    linear_addr_start = linear_addr_start + min(phys_addr_offset , linear_addr_offset);
-    phys_addr_start   = phys_addr_start   + min(phys_addr_offset , linear_addr_offset);
+    printf("aligned_phys_addr   : 0x%-13llx ~ 0x%-13llx (sz=0x%llx)\n" , aligned_phys_addr , aligned_phys_end , aligned_phys_end-aligned_phys_addr);
+    printf("padding             : 0x%llx\n" , alignment_padding_phys);
+    printf("aligned_linear_addr : 0x%-13llx ~ 0x%-13llx (sz=0x%llx)\n" , aligned_linear_addr , aligned_linear_end , aligned_linear_end-aligned_linear_addr);
+    printf("padding             : 0x%llx\n" , alignment_padding_linear);
 
-    printf("phys_addr_offset   : 0x%-12llx(%lldkB)  phys_addr_aligned   : 0x%-12llx\n" , phys_addr_offset , phys_addr_offset/1024 , phys_addr_start+phys_addr_offset);
-    printf("linear_addr_offset : 0x%-12llx(%lldkB)  linear_addr_aligned : 0x%-12llx\n" , linear_addr_offset , linear_addr_offset/1024 , linear_addr_start+linear_addr_offset);
-    printf("wasted memory      : %lldkB\n" , (max(phys_addr_offset , linear_addr_offset)-min(phys_addr_offset , linear_addr_offset))/1024);
+    max_t page_count = (aligned_linear_end-aligned_linear_addr)/page_size;
+    map_pages(linear_addr_start+alignment_padding_linear , page_size , page_count , phys_addr_start+alignment_padding_phys);
 
-    printf("smallest possible page size : 0x%llx(%dth from the hierarchy)\n" , levels_of_page_size[smallest_possible_ps_idx] , smallest_possible_ps_idx);
 
-    // linear_addr_start + 
-    // map_pages(linear_addr_start , larger_page_size ,  , phys_addr_start);
-
-    // figure_out_page_mapping_alignment(phys_addr_start , phys_addr_end , linear_addr_start);
+    printf(" =========== upper unaligned area mapping ============ \n");
+    figure_out_page_mapping_alignment(aligned_phys_end , phys_addr_end , aligned_linear_end);
+    printf(" ========== upper unaligned area mapping end ========= \n");
+    return 0;
 }
 
 int main(void) {
-    int N;
-    scanf("%d" , &N);
-    for(int i = 0; i < N; i++) {
-        max_t addr_start, addr_end;
-        max_t linear_addr_start;
-        
-        scanf("%llx %llx %llx" , &addr_start , &addr_end , &linear_addr_start);
-        printf("Mapping physical address 0x%-10llx ~ 0x%-10llx --> 0x%10llx ~ 0x%10llx\n" , addr_start , addr_end , linear_addr_start , linear_addr_start + (addr_end-addr_start));
-        figure_out_page_mapping_alignment(addr_start , addr_end , linear_addr_start);
-    }
+    figure_out_page_mapping_alignment(0x1c02000 , 0x39edf000 , 0xffaa0721000);
 
 }

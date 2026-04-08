5b3c251d1f6e543e72f8a26ce8468f0fe7721ba8
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-04-07 20:14:18 -0500

    Added ARCH_MAXIMUM_PAGE_SIZE

--- kernel/include/kernel/essentials.hpp
+++ kernel/include/kernel/essentials.hpp
@@ -17,6 +17,15 @@
 #define min(X , Y) ((X) >= (Y) ? (Y) : (X))
 #define max(X , Y) ((X) >= (Y) ? (X) : (Y))
 
+constexpr max_t ARCH_MAXIMUM_PAGE_SIZE = 
+#if CONFIG_USE_ENORMOUS_PAGE == yes
+    CONFIG_ENORMOUS_PAGE_SIZE;
+#elif CONFIG_USE_LARGE_PAGE == yes
+    CONFIG_LARGE_PAGE_SIZE;
+#else 
+    CONFIG_PAGE_SIZE;
+#endif
+
 #define __no_sanitize_address__ __attribute__((no_sanitize("address")))
 
 #endif

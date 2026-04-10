acf360bae731042c14626ea14b3cadaf1a23ee99
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-04-09 23:52:59 -0500

    Experimentally enbled KASAN in the compiler option

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -15,7 +15,7 @@ KERNEL_COMPILER = x86_64-elf
 
 # To-do : integrate conf file with makefile configurations
 KASAN_OFFSET  = 0xffaa000000000
-KASAN_OPTIONS = # -fsanitize=kernel-address \
+KASAN_OPTIONS = -fsanitize=kernel-address \
 	            -mllvm -asan-stack=1 \
 				-mllvm -asan-globals=1 \
 	            -mllvm -asan-instrumentation-with-call-threshold=0 \
@@ -23,7 +23,7 @@ KASAN_OPTIONS = # -fsanitize=kernel-address \
 COMMON_CCOPTIONS = --target=x86_64-pc-linux-gnu -march=x86-64 -ffreestanding -fno-builtin -nostdlib -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -nostdinc++ -fpack-struct=1 -masm=intel -std=c++20 -Werror=return-type -Wno-incompatible-library-redeclaration -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -O0 -g -mcmodel=large
 
 KERNEL_SETUP_CCOPTIONS =  -fno-stack-protector -fno-pic -fno-pie
-KERNEL_CCOPTIONS       =  -fPIC $(KASAN_OPTIONS)
+KERNEL_CCOPTIONS       =  $(KASAN_OPTIONS)
 
 KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding
 KERNEL_ASOPTIONS = 

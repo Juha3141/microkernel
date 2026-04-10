e550c37271dfc985d7d8c8b7b1e3aa526887a52c
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-04-10 17:18:30 -0500

    We really need a system to synchronize kasan offset

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -14,16 +14,16 @@ KERNEL_COMPILER = x86_64-elf
 -fno-rtti -fno-exceptions -Wno-write-strings
 
 # To-do : integrate conf file with makefile configurations
-KASAN_OFFSET  = 0xffaa000000000
-KASAN_OPTIONS = -fsanitize=kernel-address \
+KASAN_OFFSET  = 0x1fa000000000
+KASAN_OPTIONS = # -fsanitize=kernel-address \
 	            -mllvm -asan-stack=1 \
 				-mllvm -asan-globals=1 \
 	            -mllvm -asan-instrumentation-with-call-threshold=0 \
 				-mllvm -asan-mapping-offset=$(KASAN_OFFSET)
-COMMON_CCOPTIONS = --target=x86_64-pc-linux-gnu -march=x86-64 -ffreestanding -fno-builtin -nostdlib -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -nostdinc++ -fpack-struct=1 -masm=intel -std=c++20 -Werror=return-type -Wno-incompatible-library-redeclaration -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -O0 -g -mcmodel=large
+COMMON_CCOPTIONS = --target=x86_64-pc-linux-gnu -march=x86-64 -ffreestanding -fno-builtin -nostdlib -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -nostdinc++ -fpack-struct=1 -masm=intel -std=c++20 -Werror=return-type -Wno-incompatible-library-redeclaration -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -g -mcmodel=large
 
-KERNEL_SETUP_CCOPTIONS =  -fno-stack-protector -fno-pic -fno-pie
-KERNEL_CCOPTIONS       =  $(KASAN_OPTIONS)
+KERNEL_SETUP_CCOPTIONS = -fno-stack-protector -fno-pic -fno-pie -fno-sanitize=kernel-address
+KERNEL_CCOPTIONS       = $(KASAN_OPTIONS)
 
 KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding
 KERNEL_ASOPTIONS = 

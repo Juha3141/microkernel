bca85fc1d96bc0d969f0447a6bef98117908df57
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-04-11 02:34:08 -0500

    Added noabort option

--- kernel/include/kernel/mem/kasan.hpp
+++ kernel/include/kernel/mem/kasan.hpp
@@ -8,16 +8,18 @@
 
 #define KASAN_NULLPTR_PROTECTION 0x500
 
-#define KASAN_INTERNALS_INTERFACE extern "C"
+#define KASAN_INTERNALS_INTERFACE __no_sanitize_address__ extern "C"
 
-#define KASAN_SHADOW_MAGIC_UNPOISONED        0x00
-#define KASAN_SHADOW_MAGIC_RESERVED          0xff
-#define KASAN_SHADOW_MAGIC_GLOBAL_REDZONE    0xf9
-#define KASAN_SHADOW_MAGIC_HEAP_HEAD_REDZONE 0xfa
-#define KASAN_SHADOW_MAGIC_HEAP_TAIL_REDZONE 0xfb
-#define KASAN_SHADOW_MAGIC_HEAP_FREE         0xfd
-#define KASAN_HEAP_HEAD_REDZONE_SIZE 16
-#define KASAN_HEAP_TAIL_REDZONE_SIZE 10
+#define KASAN_SHADOW_MAGIC_UNPOISONED          0x00
+#define KASAN_SHADOW_MAGIC_RESERVED            0xff
+#define KASAN_SHADOW_MAGIC_GLOBAL_REDZONE      0xf9
+#define KASAN_SHADOW_MAGIC_HEAP_HEAD_REDZONE   0xfa
+#define KASAN_SHADOW_MAGIC_HEAP_TAIL_REDZONE   0xfb
+#define KASAN_SHADOW_MAGIC_HEAP_FREE           0xfd
+#define KASAN_SHADOW_MAGIC_STACK_AFTER_RETURN  0xf5
+#define KASAN_SHADOW_MAGIC_STACK_AFTER_SCOPE   0xf8
+#define KASAN_HEAP_HEAD_REDZONE_SIZE 8
+#define KASAN_HEAP_TAIL_REDZONE_SIZE 8
 
 namespace kasan {
     typedef max_t uptr;
@@ -27,14 +29,14 @@ namespace kasan {
     void poison_address(max_t linear_address , max_t size , byte value);
     void unpoison_address(max_t linear_address , max_t size);
     
-    bool check_address_validity(max_t linear_address , max_t size , byte is_write , max_t pc);
+    bool check_address_validity(max_t linear_address , max_t size , byte is_write , max_t pc , bool noabort=false);
     /// @brief KASan bug report function
     /// @param addr 
     /// @param size 
     /// @param buggy_shadow_address 
     /// @param is_write 
     /// @param pc Program Counter
-    void report_bug(max_t addr , max_t size , max_t buggy_shadow_address , byte is_write , max_t pc);
+    void report_bug(max_t addr , max_t size , max_t buggy_shadow_address , byte is_write , max_t pc , bool noabort=false);
 }
 
 #endif

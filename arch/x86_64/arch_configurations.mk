9a06eae70077568c805cc08023b2336db3375bfa
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-04-15 00:12:43 -0500

    Update arch_configurations.mk

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -26,10 +26,10 @@ COMMON_CCOPTIONS += -march=x86-64
 COMMON_CCOPTIONS += -ffreestanding -fno-builtin -nostdlib -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -nostdinc++ 
 COMMON_CCOPTIONS += -fpack-struct=1 -masm=intel -std=c++20 
 COMMON_CCOPTIONS += -Werror=return-type -Wno-incompatible-library-redeclaration 
-COMMON_CCOPTIONS += -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -g -mcmodel=large -W -Wall
+COMMON_CCOPTIONS += -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -g -mcmodel=large -W -Wall 
 
 KERNEL_SETUP_CCOPTIONS = -fno-stack-protector -fno-pic -fno-pie -fno-sanitize=kernel-address
-KERNEL_CCOPTIONS       = $(KASAN_OPTIONS)
+KERNEL_CCOPTIONS       = $(KASAN_OPTIONS) -fpic -fpie
 
-KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding
+KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding 
 KERNEL_ASOPTIONS = 

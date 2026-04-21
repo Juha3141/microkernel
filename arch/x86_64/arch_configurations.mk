ef155e7937f1d906cef98341320967151bfa8207
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-04-20 21:54:56 -0500

    Update arch_configurations.mk

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -24,12 +24,12 @@ KASAN_OPTIONS += -mllvm -asan-mapping-offset=$(KASAN_OFFSET)
 COMMON_CCOPTIONS := --target=x86_64-pc-linux-gnu
 COMMON_CCOPTIONS += -march=x86-64 
 COMMON_CCOPTIONS += -ffreestanding -fno-builtin -nostdlib -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -nostdinc++ 
-COMMON_CCOPTIONS += -fpack-struct=1 -masm=intel -std=c++20 
+COMMON_CCOPTIONS += -fpack-struct=1 -masm=intel -std=c++20 -fno-stack-protector
 COMMON_CCOPTIONS += -Werror=return-type -Wno-incompatible-library-redeclaration 
 COMMON_CCOPTIONS += -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -g -mcmodel=large -W -Wall 
 
-KERNEL_SETUP_CCOPTIONS = -fno-stack-protector -fno-pic -fno-pie -fno-sanitize=kernel-address
-KERNEL_CCOPTIONS       = $(KASAN_OPTIONS) -fpic -fpie
+KERNEL_SETUP_CCOPTIONS = -fno-pic -fno-pie -fno-sanitize=kernel-address
+KERNEL_CCOPTIONS       = $(KASAN_OPTIONS)
 
 KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding 
 KERNEL_ASOPTIONS = 

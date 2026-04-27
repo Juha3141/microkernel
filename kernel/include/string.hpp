0d49acefab2fbb5457280513a7ceeb3a15c6c370
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-04-26 23:40:08 -0500

    Added prototype of kernel string object

--- kernel/include/string.hpp
+++ kernel/include/string.hpp
@@ -5,6 +5,35 @@
 #include <kernel/essentials.hpp>
 #include <pair.hpp>
 
+class String {
+public:
+    String() = default;
+    String(const String& str_obj);
+    String(size_t len);
+    String(const char *str);
+
+    size_t size() const;
+    size_t max_size() const;
+    bool empty() const;
+
+    ///////// Change in capacity ///////// 
+    void reserve(size_t sz);
+    void clear();
+
+    const char *c_str();
+
+    char& operator[](int64_t idx);
+    String& operator+=(const String& str);
+    String& operator+=(const char *str);
+
+    String& substr(int idx , int size);
+    String& substr(int idx);
+private:
+    char *c_string;
+    size_t length;
+    size_t capacity;
+};
+
 extern "C" void *memset(void *dest , int c , size_t n);
 extern "C" void *memcpy(void *dest , const void *src , size_t n);
 extern "C" void *memmove(void *dest , const void *src , size_t n);
@@ -18,7 +47,7 @@ extern "C" int unsanitized_memcmp(const void *s1 , const void *s2 , size_t n);
 size_t strlen(const char *s);
 char *strcpy(char *dest , const char *src);
 char *strncpy(char *dest , const char *src , size_t n);
-char *strcat(char *dest , char *src);
+char *strcat(char *dest , const char *src);
 char *strncat(char *dest , const char *src , size_t n);
 int strcmp(const char *s1 , const char *s2);
 int strncmp(const char *s1 , const char *s2 , size_t n);

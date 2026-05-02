a0ae580cd87772b4b9f9b5d940fe27f72ba28f8d
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-05-02 12:28:49 -0500

    Create general_input_system.hpp

---
+++ kernel/include/kernel/driver/general_input_system.hpp
@@ -0,0 +1,18 @@
+#ifndef _GENERAL_INPUT_SYSTEM_HPP_
+#define _GENERAL_INPUT_SYSTEM_HPP_
+
+#include <kernel/driver/device_driver.hpp>
+#include <kernel/driver/char_device_driver.hpp>
+
+struct InputEvent {
+    
+};
+
+namespace input {
+    class InputDeviceManager {
+        
+    };
+    void register_input_device();
+}
+
+#endif

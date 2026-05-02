9a6f090a2041eac7c12f561bb273e3b16eb603f8
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-05-02 12:27:58 -0500

    Added unions of device driver registration/query functions

--- kernel/src/driver/device_driver.cpp
+++ kernel/src/driver/device_driver.cpp
@@ -1,15 +1,47 @@
 #include <kernel/driver/device_driver.hpp>
 
-#define CALL_FPTR_FROM_SECTION(section_start , section_end) for(qword func_ptr_ptr = (qword)&section_start; func_ptr_ptr < (qword)&section_end; func_ptr_ptr += sizeof(driver_init_func_ptr_t)) { \
-        qword func_ptr = *((qword *)func_ptr_ptr); \
-        ((void(*)(void))func_ptr)(); \
-    }
-
 extern qword __drivers_init_start__;
 extern qword __drivers_init_end__;
 
 extern qword __fs_drivers_init_start__;
 extern qword __fs_drivers_init_end__;
 
-void register_file_system_drivers(void) { CALL_FPTR_FROM_SECTION(__fs_drivers_init_start__ , __fs_drivers_init_end__); }
-void register_kernel_drivers(void)      { CALL_FPTR_FROM_SECTION(__drivers_init_start__ , __drivers_init_end__); }
+void dev::register_file_system_drivers(void) { CALL_FPTR_FROM_SECTION(__fs_drivers_init_start__ , __fs_drivers_init_end__); }
+void dev::register_kernel_drivers(void)      { CALL_FPTR_FROM_SECTION(__drivers_init_start__ , __drivers_init_end__); }
+
+FixedArray<device_driver *>* device_driver_container;
+
+void dev::init(void) {
+    // BlockDeviceDriverContainer : global container for kernel
+    device_driver_container = memory::new_global_object<FixedArray<device_driver*>>();
+    device_driver_container->init(512);
+}
+
+///@brief some bridge-like functions(just basic stuff)
+device_driver *dev::search_driver(const char *driver_name) { 
+    max_t id = device_driver_container->search(
+        [driver_name](device_driver *&driver) { return (bool)(strcmp(driver->driver_name , driver_name) == 0); }
+    );  
+    return device_driver_container->get(id);
+}
+
+device_driver *dev::search_driver(max_t driver_id) { return device_driver_container->get(driver_id); }
+
+max_t dev::discard_driver(const char *driver_name) { return device_driver_container->discard(search_driver(driver_name)); }
+max_t dev::discard_driver(max_t driver_id) { return device_driver_container->discard(device_driver_container->get(driver_id)); }
+
+/// @brief Other forms of register_device
+max_t dev::register_device(const char *driver_name , general_device *device) { return dev::register_device(search_driver(driver_name) , device); }
+max_t dev::register_device(max_t driver_id , general_device *device) {
+    device_driver *driver = device_driver_container->get(driver_id);
+    if(driver == nullptr) {
+        debug::out::printf(DEBUG_WARNING , "Unable to find block device driver with driver_id=%d\n" , driver_id);
+        return INVALID;
+    }
+    return dev::register_device(driver , device);
+}
+
+general_device *dev::search_device(max_t driver_id , max_t device_id) { return search_driver(driver_id)->device_container->get(driver_id); }
+general_device *dev::search_device(const char *driver_name , max_t device_id) { return dev::search_device(search_driver(driver_name) , device_id); }
+
+bool dev::discard_device(general_device *device) { return device->driver->device_container->discard(device);  }

e86694336c448ff3f95b6b71c4c53640a8846739
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-05-09 12:58:44 -0500

    Update ramdisk.hpp

--- integrated/ramdisk/ramdisk.hpp
+++ integrated/ramdisk/ramdisk.hpp
@@ -8,8 +8,8 @@ struct ramdisk_driver : public block_device_driver {
     static struct block_device *create(max_t total_sector_count , max_t bytes_per_sectors , max_t physical_addr = 0x00);
     bool prepare(void) override;
     
-    bool open(block_device *device);
-    bool close(block_device *device);
+    bool open(block_device *device) override;
+    bool close(block_device *device) override;
     max_t read(block_device *device , max_t sector_address , max_t count , void *buffer) override;
     max_t write(block_device *device , max_t sector_address , max_t count , void *buffer) override;
     bool get_geometry(block_device *device , device_geometry &geometry) override;

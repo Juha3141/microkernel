85f23548db6ee95af2b6c7ce1ed41cd686a0f2b3
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-05-09 13:00:22 -0500

    Fixed major bugs in partition detection

--- kernel/src/vfs/storage_system.cpp
+++ kernel/src/vfs/storage_system.cpp
@@ -1,6 +1,7 @@
 #include <kernel/vfs/storage_system.hpp>
-#include <kernel/driver/block_device_driver.hpp>
 #include <kernel/vfs/partition_driver.hpp>
+#include <kernel/vfs/file_system_driver.hpp>
+#include <kernel/vfs/file_system_driver.hpp>
 
 bool storage_system::detect_partitions(block_device *device) {
     max_t partitiondrv_id = identify_partition_driver(device);
@@ -35,9 +36,20 @@ void storage_system::add_logical_device(block_device_driver *driver , block_devi
     new_logical_device->driver = logical_driver;
     new_logical_device->id = device->id;
     memcpy(&new_logical_device->resources , &device->resources , sizeof(device_resources));
+
+    new_logical_device->geometry.lba_total_block_count = partition_info.physical_sector_end-partition_info.physical_sector_start;
+    new_logical_device->geometry.block_size = device->geometry.block_size;
+    new_logical_device->geometry.cylinder = device->geometry.cylinder;
+    new_logical_device->geometry.head     = device->geometry.head;
+    new_logical_device->geometry.sector   = device->geometry.sector;
+    new_logical_device->geometry.is_chs   = device->geometry.is_chs;
+
     memcpy(&new_logical_device->storage_info.partition_info , &partition_info , sizeof(partition_info_t));
-    
+    new_logical_device->storage_info.storage_type = storage_logical;
     new_logical_device->storage_info.partition_id = device->storage_info.logical_block_devs->add(new_logical_device);
+
+    debug::out::printf("Detecting partition for logical device %s%d, %d\n" , driver->driver_name , device->id , new_logical_device->storage_info.partition_id);
+    new_logical_device->storage_info.fs_driver = fsdev::detect_fs(new_logical_device);
 }
 
 block_device *storage_system::get_physical_super_device(block_device *logical_device) {

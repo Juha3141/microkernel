6d52248facb20b41e0b0548beb64ce2abe31cbd0
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-05-09 12:52:49 -0500

    Fixed weird bug in linking

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -24,7 +24,7 @@ KASAN_OPTIONS += -mllvm -asan-mapping-offset=$(KASAN_OFFSET)
 COMMON_CCOPTIONS := --target=x86_64-pc-linux-gnu
 COMMON_CCOPTIONS += -march=x86-64 -Werror=vla-cxx-extension
 COMMON_CCOPTIONS += -ffreestanding -fno-builtin -nostdlib -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -nostdinc++ 
-COMMON_CCOPTIONS += -fno-pic -fno-pie -fno-common
+COMMON_CCOPTIONS += -fno-common -fno-pie -fno-PIE
 COMMON_CCOPTIONS += -fpack-struct=1 -masm=intel -std=c++20 -fno-stack-protector
 COMMON_CCOPTIONS += -Werror=return-type -Wno-incompatible-library-redeclaration 
 COMMON_CCOPTIONS += -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -g -mcmodel=large
@@ -32,5 +32,5 @@ COMMON_CCOPTIONS += -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsaf
 KERNEL_SETUP_CCOPTIONS = -fno-sanitize=kernel-address
 KERNEL_CCOPTIONS       = $(KASAN_OPTIONS)
 
-KERNEL_LDOPTIONS = -nostartfiles -nodefaultlibs --target=x86_64-elf -nostdlib -ffreestanding -z noexecstack
+KERNEL_LDOPTIONS = -m elf_x86_64 -nostdlib -z noexecstack
 KERNEL_ASOPTIONS = 

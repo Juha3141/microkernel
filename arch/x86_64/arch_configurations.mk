e289aa68914b9283a127267c7e30a26f8ca41959
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-05-12 18:55:24 -0500

    Update arch_configurations.mk

--- arch/x86_64/arch_configurations.mk
+++ arch/x86_64/arch_configurations.mk
@@ -24,7 +24,7 @@ KASAN_OPTIONS += -mllvm -asan-mapping-offset=$(KASAN_OFFSET)
 COMMON_CCOPTIONS := --target=x86_64-pc-linux-gnu
 COMMON_CCOPTIONS += -march=x86-64 -Werror=vla-cxx-extension
 COMMON_CCOPTIONS += -ffreestanding -fno-builtin -nostdlib -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -nostdinc++ 
-COMMON_CCOPTIONS += -fno-common -fno-pie -fno-PIE
+COMMON_CCOPTIONS += -fno-common -fno-pie -fno-PIE -fno-pic -fno-PIC
 COMMON_CCOPTIONS += -fpack-struct=1 -masm=intel -std=c++20 -fno-stack-protector
 COMMON_CCOPTIONS += -Werror=return-type -Wno-incompatible-library-redeclaration 
 COMMON_CCOPTIONS += -fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-threadsafe-statics -g -mcmodel=large

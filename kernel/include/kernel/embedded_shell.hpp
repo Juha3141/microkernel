f8f8ee80cf35d5f87a924436e039928c52be5df7
Ian Juha Cho <ianisnumber2027@gmail.com>
2026-05-13 20:22:24 -0500

    Implemented embedded shell

---
+++ kernel/include/kernel/embedded_shell.hpp
@@ -0,0 +1,40 @@
+#ifndef _EMBEDDED_SHELL_HPP_
+#define _EMBEDDED_SHELL_HPP_
+
+#include <kernel/mem/kmem_manager.hpp>
+#include <kernel/mem/segmentation.hpp>
+#include <kernel/mem/pages_manager.hpp>
+#include <kernel/mem/kasan.hpp>
+
+#include <kernel/interrupt/interrupt.hpp>
+#include <kernel/interrupt/exception.hpp>
+#include <kernel/io_port.hpp>
+#include <kernel/driver/device_driver.hpp>
+#include <kernel/driver/pci.hpp>
+
+#include <kernel/input/general_input_system.hpp>
+
+#include <ramdisk/ramdisk.hpp>
+#include <kernel/vfs/storage_system.hpp>
+#include <kernel/vfs/file_system_driver.hpp>
+#include <kernel/vfs/virtual_file_system.hpp>
+
+#include <kernel/debug.hpp>
+
+#include <kernel/sections.hpp>
+#include <loader/loader_argument.hpp>
+
+#include <kernel/init.hpp>
+
+namespace eshell {
+    void start();
+
+    namespace cmd {
+        int cd(file_info* &current_dir , int argc , char **argv);
+        int ls(file_info* &current_dir , int argc , char **argv);
+        int clear(file_info* &current_dir , int argc , char **argv);
+        int echo(file_info* &current_dir , int argc , char **argv);
+    }
+}
+
+#endif
---
+++ kernel/src/embedded_shell.cpp
@@ -0,0 +1,189 @@
+#include <kernel/embedded_shell.hpp>
+
+static void read_input(InputReader& kbd_reader , String &str) {
+    str.reserve(128);
+    while(1) {
+        input_event event;
+        if(!kbd_reader.read(event)) continue;
+        // ignore any other input types for now
+        if(event.type != INPUT_TYPE_KEYDOWN) continue;
+        
+        byte data = event.data & 0xff;
+        if(data == '\n') {
+            debug::out::printf("%c" , data);
+            break;
+        }
+        else if(data == '\t') {
+            /* Action reserved for later */
+        }
+        else if(data == '\b') {
+            if(!str.backspace()) continue;
+            // Remove one character from the screen
+            debug::out::printf("\b \b");
+        }
+        else {
+            str += data;
+            debug::out::printf("%c" , data);
+        }
+    }
+    // remove the unnecessary spaces at the rear of the string
+    for(int i = str.size()-1; i >= 0; i--) {
+        if(str[i] == ' ') str[i] = '\0';
+        else break;
+    }
+}
+
+static bool parse_arguments(const String& full_str , LinkedList<String*>&ll) {
+    bool success = true;
+    max_t prev_idx = 0;
+    for(max_t i = 0; full_str[i] != 0; i++) {
+        if(full_str[i] == ' ') {
+            if(i == prev_idx) { prev_idx = i+1; continue; }
+
+            String *new_str = new String(full_str.substr(prev_idx , i-prev_idx));
+            ll.add_rear(new_str);
+            prev_idx = i+1;
+        }
+    }
+    ll.add_rear(new String(full_str.substr(prev_idx)));
+    return success;
+}
+
+static void release_str_ll(LinkedList<String*>&ll) {
+    auto *ptr = ll.get_start_node();
+    while(ptr != nullptr) {
+        delete ptr->object;
+
+        ptr = ptr->next;
+    }
+}
+
+typedef int (*shell_command_func_t)(file_info* &current_dir , int argc , char **argv);
+
+struct shell_commands_t {
+    const char *cmd;
+    const char *help;
+
+    shell_command_func_t func;    
+};
+
+shell_commands_t commands_list[] = {
+    {"cd" , "enter a directory" , eshell::cmd::cd} , 
+    {"ls" , "list files in the current directory" , eshell::cmd::ls} , 
+    {"clear" , "clears the screen" , eshell::cmd::clear} , 
+    {"echo" , "echo" , eshell::cmd::echo} , 
+};
+
+void eshell::start() {
+    debug::out::printf("Entering the Embedded Shell\n");
+    
+    InputReader kbd_reader;
+    kbd_reader.open("keyboard");
+    String current_dir_name("@");
+    file_info *current_dir_f = vfs::open({current_dir_name.c_str() , nullptr} , FILE_OPEN_READONLY);
+    
+    while(1) {
+        String str;
+        vfs::get_full_filename(current_dir_f , current_dir_name);
+        debug::out::printf("eshell: %s > " , current_dir_name.c_str());
+        read_input(kbd_reader , str);
+
+        LinkedList<String*>parsed_arguments;
+        parse_arguments(str , parsed_arguments);
+
+        if(parsed_arguments.size() == 0) continue;
+
+        auto *ptr = parsed_arguments.get_start_node();
+
+        // construct argv for commands
+        char **argv = (char **)memory::pmem_alloc(parsed_arguments.size()*sizeof(char *));
+        max_t i = 0;
+        while(ptr != nullptr) {
+            debug::out::printf("arg %d : \"%s\"\n" , i , ptr->object->c_str());
+            argv[i++] = (char *)ptr->object->c_str();
+            ptr = ptr->next;
+        }
+        /**** Handle the command ****/
+        int ret = 0x7fffffff;
+        for(int i = 0; i < sizeof(commands_list)/sizeof(shell_commands_t); i++) {
+            if(strcmp(commands_list[i].cmd , argv[0]) == 0) {
+                ret = commands_list[i].func(current_dir_f , parsed_arguments.size() , argv);
+                break;
+            }
+        }
+        if(ret == 0x7fffffff) {
+            debug::out::printf("Command named \"%s\" does not exist\n" , argv[0]);
+        }
+
+        memory::pmem_free(argv);
+        release_str_ll(parsed_arguments);
+    }
+}
+
+int eshell::cmd::cd(file_info* &current_dir , int argc , char **argv) {
+    if(argc > 2) debug::out::printf("cd : too many arguments!\n");
+    if(argc == 1) {
+        String full_name;
+        vfs::get_full_filename(current_dir , full_name);
+        debug::out::printf("%s\n\n" , full_name.c_str());
+        return 0;
+    }
+
+    file_info *new_info = vfs::open({argv[1] , current_dir} , FILE_OPEN_READONLY);
+    if(new_info == nullptr) {
+        new_info = vfs::open({argv[1] , nullptr} , FILE_OPEN_READONLY);
+    }
+    debug::out::printf("new_info : 0x%llx\n" , new_info);
+    if(new_info == nullptr) {
+        debug::out::printf("\"%s\" : No such directory\n" , argv[1]);
+        return -1;
+    }
+    current_dir = new_info;
+    return 0;
+}
+
+int eshell::cmd::ls(file_info* &current_dir , int argc , char **argv) {
+    debug::out::printf("ls, current_dir : %s\n" , current_dir->file_name);
+    int file_count = vfs::read_directory(current_dir);
+    debug::out::printf("%d files\n" , file_count);
+    max_t max_file_name_len = 0;
+    
+    auto ptr = current_dir->file_list->get_start_node();
+    while(ptr != nullptr) {
+        max_file_name_len = max(strlen(ptr->object->file_name) , max_file_name_len);
+        ptr = ptr->next;
+    }
+
+    ptr = current_dir->file_list->get_start_node();
+    while(ptr != nullptr) {
+        max_t f_len = strlen(ptr->object->file_name);
+        debug::out::printf("%s" , ptr->object->file_name);
+        for(int i = 0; i < max_file_name_len-f_len+3; i++) { debug::out::printf(" "); }
+
+        if(ptr->object->file_type == FILE_TYPE_DIRECTORY) {
+            debug::out::printf("DIR\n");
+        }
+        else if(ptr->object->file_type == FILE_TYPE_DEVICE_FILE) {
+            debug::out::printf("DEV\n");
+        }
+        else {
+            debug::out::printf("%lld(%d)\n" , ptr->object->file_size , ptr->object->file_type);
+        }
+        ptr = ptr->next;
+    }
+    return 0;
+}
+
+int eshell::cmd::clear(file_info* &current_dir , int argc , char **argv) {
+    debug::out::clear_screen();
+    return 0;
+}
+
+int eshell::cmd::echo(file_info* &current_dir , int argc , char **argv) {
+    for(int i = 1; i < argc; i++) {
+        debug::out::printf("%s" , argv[i]);
+        if(i != argc-1) debug::out::printf(" ");
+    }
+    debug::out::printf("\n");
+    return 0;
+}
